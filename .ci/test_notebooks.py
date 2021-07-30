790bf69196aa9ce248bdd691ad5718d446b3e7eb
Helena <helena.kloosterman@intel.com>
2021-07-30 14:29:21 +0200

    Standardize path for test_notebooks

--- .ci/test_notebooks.py
+++ .ci/test_notebooks.py
@@ -4,8 +4,8 @@ def test_readme():
     """
     Test that all notebooks have a README file and exist in the Notebooks README
     """
-    notebooks_readme = Path("../notebooks/README.md").read_text()
-    for item in Path("../notebooks").iterdir():
+    notebooks_readme = Path("notebooks/README.md").read_text()
+    for item in Path("notebooks").iterdir():
         if item.is_dir() and str(item)[0].isdigit():
         # item is a notebook directory
             assert "README.md" in [filename.name for filename in item.iterdir()], \
