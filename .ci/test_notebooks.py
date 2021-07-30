3c4dd328762a1bb8f0f4a8d945ba7df4c720dc9d
Helena <helena.kloosterman@intel.com>
2021-07-30 14:18:22 +0200

    Add CI test that checks that notebooks have a README
    Also checks that notebooks are included in main notebooks README

---
+++ .ci/test_notebooks.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+def test_readme():
+    """
+    Test that all notebooks have a README file and exist in the Notebooks README
+    """
+    notebooks_readme = Path("../notebooks/README.md").read_text()
+    for item in Path("../notebooks").iterdir():
+        if item.is_dir() and str(item)[0].isdigit():
+        # item is a notebook directory
+            assert "README.md" in [filename.name for filename in item.iterdir()], \
+                   f"README not found in {item}"
+            assert str(item) in notebooks_readme, f"{item} not found in notebooks README"
