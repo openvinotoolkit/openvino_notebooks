1042964fb1e175410db47f1e910802dc106c5d7f
Helena <helena.kloosterman@intel.com>
2021-08-02 13:38:46 +0200

    Revert README test

--- .ci/test_notebooks.py
+++ .ci/test_notebooks.py
@@ -8,7 +8,6 @@ def test_readme():
     for item in Path("notebooks").iterdir():
         if item.is_dir() and str(item)[0].isdigit():
         # item is a notebook directory
-            print(item)
             assert "README.md" in [filename.name for filename in item.iterdir()], \
                    f"README not found in {item}"
             assert str(item) in notebooks_readme, f"{item} not found in notebooks README"
