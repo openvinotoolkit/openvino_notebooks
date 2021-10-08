15ae4cb97251b8a683e9641891cf3fefb08357d8
Jakub Debski <jakub.debski@intel.com>
2021-10-08 14:55:52 +0200

    Add comment

--- .ci/convert_notebooks.sh
+++ .ci/convert_notebooks.sh
@@ -8,6 +8,7 @@ mkdir -p $rstdir
 mkdir -p $htmldir
 mkdir -p $markdowndir
 
+# List all notebooks that contain binder buttons based on readme
 cat README.md | grep mybinder.org | awk -e '/[0-9]{3}/' | cut -f1 -d] | cut -f2 -d[ > $binderlist
 
 git ls-files "*.ipynb" | while read notebook; do
