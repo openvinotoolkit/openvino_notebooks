137762f3e1b5c5b08f52305c73ca347123b53975
Adrian Boguszewski <adrian.boguszewski@intel.com>
2022-08-09 15:39:19 +0100

    Remove only created files

--- .ci/validate_notebooks.sh
+++ .ci/validate_notebooks.sh
@@ -1,24 +1,20 @@
-ignorelist=""
+ignore_list=$*
 failed=0
-earlystop=false
+early_stop=false
 
-for var in "$@"
+for notebook in notebooks/*/;
 do
-    ignorelist="$ignorelist $var"
-done
-
-for notebook in $(git ls-files "*.ipynb");
-do
-    notebookpath=${notebook%/*}
-    notebookdir="$(echo "$notebook" | rev | cut -d'/' -f2 | rev)"
-    if echo "$ignorelist" | grep -w -q "$notebookdir"; then
-        continue
+    cd "$notebook" || exit 1
+    existing_files=$(find . | sort)
+    notebook_name=$(echo "$notebook" | cut -d'/' -f2)
+    # if notebook not ignored
+    if ! echo "$ignore_list" | grep -w -q "$notebook_name"; then
+        python -m pytest --nbval -k "test_ or notebook_utils" --durations 10
+        failed=$((failed | $?))
+        comm -23 <(find . | sort) <(echo "$existing_files") | xargs rm -rf
     fi
-    python -m pytest --nbval "$notebook" --durations 10
-    failed=$((failed | $?))
-    rm -r "$notebookpath"/data/*
-    rm -r "$notebookpath"/model/*
-    if [ "$earlystop" = true ] && [ $failed -ge 1 ]; then
+    cd - || exit 1
+    if [ "$early_stop" = true ] && [ $failed -ge 1 ]; then
         break
     fi
 done
