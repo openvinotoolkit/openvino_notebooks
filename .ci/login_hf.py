b51562591733a5c4da26811df894a0a6b32e45ec
Aidova <ekaterina.aidova@intel.com>
2022-11-02 14:43:14 +0400

    meter reader upd

--- .ci/login_hf.py
+++ .ci/login_hf.py
@@ -1,15 +1,12 @@
 from argparse import ArgumentParser
-from huggingface_hub import HfApi
 from huggingface_hub.commands.user import _login
-from huggingface_hub import HfFolder
-
+from huggingface_hub.hf_api import HfApi
 
 def main():
     parser = ArgumentParser()
     parser.add_argument("--token", required=True)
     args = parser.parse_args()
-    _login(HfApi(), token=args.token)
-    HfFolder.save_token(args.token)
+    _login(hf_api=HfApi(), token=args.token)
 
 if __name__ == "__main__":
     main()
