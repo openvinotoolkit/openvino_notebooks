d9505a0e11b1acbadcc232e056c4b69513a4d2f9
Ethan Yang <ethan.yang@intel.com>
2023-07-03 18:46:40 +0800

    [Recipe]upload onnx pretrained models (#1162)
    * upload onnx models
    
    * fix talidation issues

--- recipes/meter_reader/analog/paddle.py
+++ recipes/meter_reader/analog/paddle.py
@@ -1,11 +1,12 @@
 from analog.base import analog_base
 import numpy as np
 import cv2
+import os
 
 
 class analog_paddle(analog_base):
-    def __init__(self, config):
-        super().__init__(config)
+    def __init__(self, config, output_dir):
+        super().__init__(config, output_dir)
 
     def detect(self, input):
         # Prepare the input data for meter detection model
@@ -36,7 +37,7 @@ class analog_paddle(analog_base):
         # Create the pictures of detection results
         roi_stack = np.hstack(resize_imgs)
 
-        cv2.imwrite("./data/detection_results.jpg", roi_stack)
+        cv2.imwrite(os.path.join(self.output_dir, "detection_results.jpg"), roi_stack)
 
         return roi_imgs
 
@@ -63,7 +64,7 @@ class analog_paddle(analog_base):
         # Create the pictures of segmentation results
         mask_stack = np.hstack(image_list)
 
-        cv2.imwrite("./data/segmentation_results.jpg",
+        cv2.imwrite(os.path.join(self.output_dir, "segmentation_results.jpg"),
                     cv2.cvtColor(mask_stack, cv2.COLOR_RGB2BGR))
 
         return seg_results
--- recipes/meter_reader/main.py
+++ recipes/meter_reader/main.py
@@ -2,6 +2,7 @@ from analog.paddle import analog_paddle
 from analog.yolo import analog_yolo
 import argparse
 import cv2
+import os
 import json
 
 if __name__ == "__main__":
@@ -14,15 +15,17 @@ if __name__ == "__main__":
     parser.add_argument('-t', '--task',  required=True, default='analog', type=str,
                       help='Required. mode of meter reader, digital or analog')
     args = parser.parse_args()
-
+    
+    output_dir = os.path.abspath(os.path.dirname(args.input))
     with open(args.config) as f:
         config = json.load(f)
     if len(config["model_config"]["detector"]["model_shape"]) == 1:
-        meter_reader = analog_yolo(config)
+        meter_reader = analog_yolo(config, output_dir)
     else:
-        meter_reader = analog_paddle(config)
+        meter_reader = analog_paddle(config, output_dir)
     image = cv2.imread(args.input)
     det_resutls = meter_reader.detect(image)
     seg_resutls = meter_reader.segment(det_resutls)
     post_resutls = meter_reader.postprocess(seg_resutls)
     meter_reader.reading(post_resutls, image)
+    print(f"result images saved to \"{output_dir}\".")
