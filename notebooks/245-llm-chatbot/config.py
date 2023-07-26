cb5bc4056850f0a2465fa8108eea4020c3f9e767
Ekaterina Aidova <ekaterina.aidova@intel.com>
2023-07-26 19:00:03 +0400

    revert changes in dolly readme

--- notebooks/245-llm-chatbot/config.py
+++ notebooks/245-llm-chatbot/config.py
@@ -23,7 +23,7 @@ def llama_partial_text_processor(partial_text, new_text):
     return partial_text
 
 SUPPORTED_MODELS = {
-  "red-pijama-3b-chat" : {"model_id": "togethercomputer/RedPajama-INCITE-Chat-3B-v1", "start_message": "", "history_template": "\n<human>:{user}\n<bot>:{assistant}", "stop_tokens": [29, 0], "partial_text_processor": red_pijama_partial_text_processor, "current_message_template": "\n<human>:{user}\n<bot>:{assistant}"},
+  "red-pajama-3b-chat" : {"model_id": "togethercomputer/RedPajama-INCITE-Chat-3B-v1", "start_message": "", "history_template": "\n<human>:{user}\n<bot>:{assistant}", "stop_tokens": [29, 0], "partial_text_processor": red_pijama_partial_text_processor, "current_message_template": "\n<human>:{user}\n<bot>:{assistant}"},
   "llama-2-chat-7b": {"model_id": "meta-llama/Llama-2-7b-hf", "start_message": f"<s>[INST] <<SYS>>\n{DEFAULT_SYSTEM_PROMPT }\n<</SYS>>\n\n", "history_template": "{user}[/INST]{assistant}</s><s>[INST]", "current_message_template": "{user} [/INST]{assistant}","tokenizer_kwargs": {"add_special_tokens":False}, "partial_text_processor": llama_partial_text_processor},
   "mpt-7b-chat": {"model_id": "mosaicml/mpt-7b-chat", "start_message": f"<|im_start|>system\n {DEFAULT_SYSTEM_PROMPT }<|im_end|>", "history_template": "<|im_start|>user\n{user}<im_end><|im_start|>assistant\n{assistant}<|im_end|>", "current_message_template": "\"<|im_start|>user\n{user}<im_end><|im_start|>assistant\n{assistant}", "stop_tokens": ["<|im_end|>", "<|endoftext|>"]},
 }
