634610d2f46fb25467514f0e4eeefcce6420059d
Nikita Savelyev <nikita.savelyev@intel.com>
2023-08-22 16:51:50 +0200

    Added more common logic to utils

--- notebooks/227-whisper-subtitles-generation/utils.py
+++ notebooks/227-whisper-subtitles-generation/utils.py
@@ -1,10 +1,116 @@
 from collections import namedtuple
-from typing import Union, List
 from functools import partial
+from openvino import Core, Tensor
+from pathlib import Path
+from typing import List, Optional, Union
 
+import numpy as np
 import torch
 
 from whisper.decoding import DecodingTask, Inference, DecodingOptions, DecodingResult
+
+
+class OpenVINOAudioEncoder(torch.nn.Module):
+    """
+    Helper for inference Whisper encoder model with OpenVINO
+    """
+
+    def __init__(self, core, model_path: Path, device='CPU'):
+        super().__init__()
+        self.model = core.read_model(model_path)
+        self.compiled_model = core.compile_model(self.model, device)
+        self.output_blob = self.compiled_model.output(0)
+
+    def forward(self, mel: torch.Tensor):
+        """
+        Inference OpenVINO whisper encoder model.
+
+        Parameters:
+          mel: input audio fragment mel spectrogram.
+        Returns:
+          audio_features: torch tensor with encoded audio features.
+        """
+        return torch.from_numpy(self.compiled_model(mel)[self.output_blob])
+
+
+class OpenVINOTextDecoder(torch.nn.Module):
+    """
+    Helper for inference OpenVINO decoder model
+    """
+
+    def __init__(self, core: Core, model_path: Path, device: str = 'CPU'):
+        super().__init__()
+        self._core = core
+        self.model = core.read_model(model_path)
+        self._input_names = [inp.any_name for inp in self.model.inputs]
+        self.compiled_model = core.compile_model(self.model, device)
+        self.device = device
+
+    def init_past_inputs(self, feed_dict):
+        """
+        Initialize cache input for first step.
+
+        Parameters:
+          feed_dict: Dictonary with inputs for inference
+        Returns:
+          feed_dict: updated feed_dict
+        """
+        beam_size = feed_dict['x'].shape[0]
+        audio_len = feed_dict['xa'].shape[2]
+        previous_seq_len = 0
+        for name in self._input_names:
+            if name in ['x', 'xa']:
+                continue
+            feed_dict[name] = Tensor(np.zeros((beam_size, previous_seq_len, audio_len), dtype=np.float32))
+        return feed_dict
+
+    def preprocess_kv_cache_inputs(self, feed_dict, kv_cache):
+        """
+        Transform kv_cache to inputs
+
+        Parameters:
+          feed_dict: dictionary with inputs for inference
+          kv_cache: dictionary with cached attention hidden states from previous step
+        Returns:
+          feed_dict: updated feed dictionary with additional inputs
+        """
+        if not kv_cache:
+            return self.init_past_inputs(feed_dict)
+        for k, v in zip(self._input_names[2:], kv_cache):
+            feed_dict[k] = Tensor(v)
+        return feed_dict
+
+    def postprocess_outputs(self, outputs):
+        """
+        Transform model output to format expected by the pipeline
+
+        Parameters:
+          outputs: outputs: raw inference results.
+        Returns:
+          logits: decoder predicted token logits
+          kv_cache: cached attention hidden states
+        """
+        logits = torch.from_numpy(outputs[0])
+        kv_cache = list(outputs.values())[1:]
+        return logits, kv_cache
+
+    def forward(self, x: torch.Tensor, xa: torch.Tensor, kv_cache: Optional[dict] = None):
+        """
+        Inference decoder model.
+
+        Parameters:
+          x: torch.LongTensor, shape = (batch_size, <= n_ctx) the text tokens
+          xa: torch.Tensor, shape = (batch_size, n_mels, n_audio_ctx)
+             the encoded audio features to be attended on
+          kv_cache: Dict[str, torch.Tensor], attention modules hidden states cache from previous steps
+        Returns:
+          logits: decoder predicted logits
+          kv_cache: updated kv_cache with current step hidden states
+        """
+        feed_dict = {'x': Tensor(x.numpy()), 'xa': Tensor(xa.numpy())}
+        feed_dict = (self.preprocess_kv_cache_inputs(feed_dict, kv_cache))
+        res = self.compiled_model(feed_dict)
+        return self.postprocess_outputs(res)
 
 
 class OpenVINOInference(Inference):
