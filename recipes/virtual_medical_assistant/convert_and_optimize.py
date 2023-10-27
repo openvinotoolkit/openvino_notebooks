59c93d4bb27cd6ff76a6863ab71abe0e3b3f8a0d
Adrian Boguszewski <adrian.boguszewski@intel.com>
2023-10-27 12:39:14 +0100

    Add possibility to quantize weights for the chat model

--- recipes/virtual_medical_assistant/convert_and_optimize.py
+++ recipes/virtual_medical_assistant/convert_and_optimize.py
@@ -1,8 +1,8 @@
 import argparse
 from pathlib import Path
 
-from optimum.intel import OVModelForCausalLM
-from transformers import AutoTokenizer
+from optimum.intel import OVModelForCausalLM, OVQuantizer
+from transformers import AutoTokenizer, AutoModelForCausalLM
 
 MODEL_MAPPING = {
     "llama2-7B": "meta-llama/Llama-2-7b-chat-hf",
@@ -10,24 +10,33 @@ MODEL_MAPPING = {
 }
 
 
-def convert_chat_model(model_type, model_dir):
+def convert_chat_model(model_type, quantize_weights, model_dir):
     """
     Convert chat model
 
     Params:
-        model_size: selected mode size
+        model_type: selected mode type and size
+        quantize_weights: whether quantize weights to INT8
         model_dir: dir to export model
     Returns:
        Path to exported model
     """
     output_dir = model_dir / model_type
     model_name = MODEL_MAPPING[model_type]
-    # load model and convert it to OpenVINO
-    model = OVModelForCausalLM.from_pretrained(model_name, export=True, compile=False)
-    # change precision to FP16
-    model.half()
-    # save model to disk
-    model.save_pretrained(output_dir)
+
+    if quantize_weights:
+        # load model
+        model = AutoModelForCausalLM.from_pretrained(model_name)
+        # change precision to INT8 and save to disk
+        quantizer = OVQuantizer.from_pretrained(model)
+        quantizer.quantize(save_directory=output_dir, weights_only=True)
+    else:
+        # load model and convert it to OpenVINO
+        model = OVModelForCausalLM.from_pretrained(model_name, export=True, compile=False)
+        # change precision to FP16
+        model.half()
+        # save model to disk
+        model.save_pretrained(output_dir)
 
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     tokenizer.save_pretrained(output_dir)
@@ -39,7 +48,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--chat_model_type", type=str, choices=["llama2-7B", "llama2-13B"],
                         default="llama2-7B", help="Chat model to be converted")
+    parser.add_argument("--quantize_weights", type=bool, default=True, help="Whether to quantize weights to INT8")
     parser.add_argument("--model_dir", type=str, default="model", help="Directory to place the model in")
 
     args = parser.parse_args()
-    convert_chat_model(args.chat_model_type, Path(args.model_dir))
+    convert_chat_model(args.chat_model_type, args.quantize_weights, Path(args.model_dir))
