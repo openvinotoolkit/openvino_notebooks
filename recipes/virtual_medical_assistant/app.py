3955161528028baab246870839f91407bac8f153
Adrian Boguszewski <adrian.boguszewski@intel.com>
2023-10-30 18:38:19 +0000

    Simple gradio interface

--- recipes/virtual_medical_assistant/app.py
+++ recipes/virtual_medical_assistant/app.py
@@ -1,7 +1,7 @@
 import argparse
 from pathlib import Path
-from typing import Tuple
 
+import gradio as gr
 from optimum.intel import OVModelForCausalLM
 from transformers import AutoConfig, AutoTokenizer, PreTrainedTokenizer
 
@@ -13,73 +13,80 @@ CHAT_MODEL_TEMPLATES = {
     }
 }
 
+chat_model: OVModelForCausalLM = None
+chat_tokenizer: PreTrainedTokenizer = None
+system_configuration: str = None
+message_template: str = None
 
-def load_chat_model(model_dir: Path) -> Tuple[OVModelForCausalLM, PreTrainedTokenizer]:
+
+def load_chat_model(model_dir: Path) -> None:
     """
-    Load chat model
+    Load chat model and assign it to a global variable
 
     Params:
         model_dir: dir with the chat model
-    Returns:
-       Chat model in OpenVINO and tokenizer
     """
+    global chat_model, chat_tokenizer, system_configuration, message_template
+
+    model_name = model_dir.name.split("-")[0]
+    # system and message templates specific to the loaded model
+    system_configuration = CHAT_MODEL_TEMPLATES[model_name]["system_configuration"]
+    message_template = CHAT_MODEL_TEMPLATES[model_name]["message_template"]
+
     ov_config = {'PERFORMANCE_HINT': 'LATENCY', 'NUM_STREAMS': '1', "CACHE_DIR": ""}
-    model = OVModelForCausalLM.from_pretrained(model_dir, device="AUTO", config=AutoConfig.from_pretrained(model_dir),
-                                               ov_config=ov_config)
-    tokenizer = AutoTokenizer.from_pretrained(model_dir)
-    return model, tokenizer
+    chat_model = OVModelForCausalLM.from_pretrained(model_dir, device="AUTO", config=AutoConfig.from_pretrained(model_dir), ov_config=ov_config)
+    chat_tokenizer = AutoTokenizer.from_pretrained(model_dir)
 
 
-def respond(prompt: str, model: OVModelForCausalLM, tokenizer: PreTrainedTokenizer) -> str:
+def respond(prompt: str) -> str:
     """
     Respond to the current prompt
 
     Params:
         prompt: user's prompt
-        model: chat model
-        tokenizer: tokenizer for the chat model
     Returns:
        The chat's response
     """
-    inputs = tokenizer(prompt, return_tensors="pt").to(model.device)
+    inputs = chat_tokenizer(prompt, return_tensors="pt").to(chat_model.device)
     input_length = inputs.input_ids.shape[1]
-    outputs = model.generate(**inputs, max_new_tokens=256, do_sample=True, temperature=0.6, top_p=0.9, top_k=50)
+    outputs = chat_model.generate(**inputs, max_new_tokens=256, do_sample=True, temperature=0.6, top_p=0.9, top_k=50)
     token = outputs[0, input_length:]
-    return tokenizer.decode(token).split("</s>")[0]
+    return chat_tokenizer.decode(token).split("</s>")[0]
 
 
-def run(chat_model_dir: Path) -> None:
+def chat(user_prompt: str, history: list) -> str:
+    messages = [message_template.format(human, ai if ai is not None else "") for human, ai in history]
+    messages.append(message_template.format(user_prompt, ""))
+    conversation = system_configuration + "\n".join(messages)
+
+    return respond(conversation)
+
+
+def run(chat_model_dir: Path, public_interface: bool = False) -> None:
     """
     Run the assistant application
 
-    Params:
+    Params
         chat_model_dir: dir with the chat model
+        public_interface: whether UI should be available publicly
     """
-    model_name = chat_model_dir.name.split("-")[0]
-    # system and message templates specific to the loaded model
-    system_configuration = CHAT_MODEL_TEMPLATES[model_name]["system_configuration"]
-    message_template = CHAT_MODEL_TEMPLATES[model_name]["message_template"]
-
     # load chat model
-    chat_model, chat_tokenizer = load_chat_model(chat_model_dir)
+    load_chat_model(chat_model_dir)
 
     history = []
-    user_prompt = "Please introduce yourself and greet the patient"
-    while True:
-        messages = [message_template.format(human, ai) for human, ai in history]
-        messages.append(message_template.format(user_prompt, ""))
-        conversation = system_configuration + "\n".join(messages)
+    initial_prompt = "Please introduce yourself and greet the patient"
+    initial_message = chat(initial_prompt, history)
 
-        output = respond(conversation, chat_model, chat_tokenizer)
-        history.append((user_prompt, output))
-        print(output)
+    chat_ui = gr.Chatbot(value=[(None, initial_message)])
+    chatbot_ui = gr.ChatInterface(fn=chat, chatbot=chat_ui)
 
-        user_prompt = input()
+    chatbot_ui.launch(share=public_interface)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--chat_model_dir', type=str, default="model/llama2-7B", help="Path to the chat model directory")
+    parser.add_argument('--public_interface', default=False, action="store_true", help="Whether interface should be available publicly")
 
     args = parser.parse_args()
-    run(Path(args.chat_model_dir))
+    run(Path(args.chat_model_dir), args.public_interface)
