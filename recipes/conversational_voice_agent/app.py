b67ab1de81615db5fc56573fa51fdef56aae9b1c
Adrian Boguszewski <adrian.boguszewski@intel.com>
2023-11-01 16:44:20 +0000

    Changed virtual assistant to conversational agent

--- recipes/conversational_voice_agent/app.py
+++ recipes/conversational_voice_agent/app.py
@@ -1,62 +1,21 @@
 import argparse
 from pathlib import Path
-from typing import Tuple, List
 
 import gradio as gr
-import librosa
-import numpy as np
-from optimum.intel import OVModelForCausalLM, OVModelForSpeechSeq2Seq
-from transformers import AutoConfig, AutoTokenizer, PreTrainedTokenizer, AutoProcessor
+from optimum.intel import OVModelForCausalLM
+from transformers import AutoConfig, AutoTokenizer, PreTrainedTokenizer
 
-from bark_utils import OVBark, SAMPLE_RATE
-
-AUDIO_WIDGET_SAMPLE_RATE = 16000
-
-SYSTEM_CONFIGURATION = "You're Adrishuo - a conversational agent. You talk to a customer. Your task is to recommend the customer products based on their needs."
-GREET_THE_CUSTOMER = "Please introduce yourself and greet the customer"
+CHAT_MODEL_TEMPLATES = {
+    "llama2": {
+        "system_configuration": "<<SYS>>\nYou're Adrishuo - a conversational agent. You talk to a customer. Your task is to recommend the customer products based on their needs.\n<</SYS>>\n\n",
+        "message_template": "[INST] {} [/INST] {}\n"
+    }
+}
 
 chat_model: OVModelForCausalLM = None
 chat_tokenizer: PreTrainedTokenizer = None
 system_configuration: str = None
 message_template: str = None
-
-tts_model: OVBark = None
-
-asr_model: OVModelForSpeechSeq2Seq = None
-asr_processor: AutoProcessor = None
-
-
-def load_asr_model(model_dir: Path) -> None:
-    """
-    Load automatic speech recognition model and assign it to a global variable
-
-    Params:
-        model_dir: dir with the ASR model
-    """
-    global asr_model, asr_processor
-
-    # create a distil-whisper model and its processor
-    asr_model = OVModelForSpeechSeq2Seq.from_pretrained(model_dir, device="AUTO")
-    asr_processor = AutoProcessor.from_pretrained(model_dir)
-
-
-def load_tts_model(model_dir: Path, speaker_type: str) -> None:
-    """
-    Load text-to-speech model and assign it to a global variable
-
-    Params:
-        model_dir: dir with the TTS model
-        speaker_type: male or female
-    """
-    global tts_model
-
-    text_encoder_path0 = model_dir / "text_encoder" / "bark_text_encoder_0.xml"
-    text_encoder_path1 = model_dir / "text_encoder" / "bark_text_encoder_1.xml"
-    coarse_encoder_path = model_dir / "coarse_model" / "bark_coarse_encoder.xml"
-    fine_model_dir = model_dir / "fine_model"
-
-    # create a bark model
-    tts_model = OVBark(text_encoder_path0, text_encoder_path1, coarse_encoder_path, fine_model_dir, device="AUTO", speaker=speaker_type)
 
 
 def load_chat_model(model_dir: Path) -> None:
@@ -68,7 +27,11 @@ def load_chat_model(model_dir: Path) -> None:
     """
     global chat_model, chat_tokenizer, system_configuration, message_template
 
-    # load llama model and its tokenizer
+    model_name = model_dir.name.split("-")[0]
+    # system and message templates specific to the loaded model
+    system_configuration = CHAT_MODEL_TEMPLATES[model_name]["system_configuration"]
+    message_template = CHAT_MODEL_TEMPLATES[model_name]["message_template"]
+
     ov_config = {'PERFORMANCE_HINT': 'LATENCY', 'NUM_STREAMS': '1', "CACHE_DIR": ""}
     chat_model = OVModelForCausalLM.from_pretrained(model_dir, device="AUTO", config=AutoConfig.from_pretrained(model_dir), ov_config=ov_config)
     chat_tokenizer = AutoTokenizer.from_pretrained(model_dir)
@@ -81,161 +44,48 @@ def respond(prompt: str) -> str:
     Params:
         prompt: user's prompt
     Returns:
-        The chat's response
+       The chat's response
     """
-    # tokenize input text
     inputs = chat_tokenizer(prompt, return_tensors="pt").to(chat_model.device)
     input_length = inputs.input_ids.shape[1]
-    # generate response tokens
     outputs = chat_model.generate(**inputs, max_new_tokens=256, do_sample=True, temperature=0.6, top_p=0.9, top_k=50)
     token = outputs[0, input_length:]
-    # decode tokens into text
     return chat_tokenizer.decode(token).split("</s>")[0]
 
 
-def chat(history: List) -> List[List[str]]:
-    """
-    Chat function. It generates response based on a prompt
+def chat(user_prompt: str, history: list) -> str:
+    messages = [message_template.format(human, ai if ai is not None else "") for human, ai in history]
+    messages.append(message_template.format(user_prompt, ""))
+    conversation = system_configuration + "\n".join(messages)
 
-    Params:
-        history: history of the messages (conversation) so far
-    Returns:
-        History with the latest chat's response
-    """
-    # the conversation must be in that format to use chat template
-    conversation = [
-        {"role": "system", "content": SYSTEM_CONFIGURATION},
-        {"role": "user", "content": GREET_THE_CUSTOMER}
-    ]
-    # add prompts to the conversation
-    for user_prompt, assistant_response in history:
-        if user_prompt:
-            conversation.append({"role": "user", "content": user_prompt})
-        if assistant_response:
-            conversation.append({"role": "assistant", "content": assistant_response})
-    # use a template specific to the model
-    conversation = chat_tokenizer.apply_chat_template(conversation, tokenize=False)
-
-    # generate response for the conversation
-    response = respond(conversation)
-    history[-1][1] = response
-
-    # return chat history as the list of message pairs
-    return history
+    return respond(conversation)
 
 
-def synthesize(conversation: List[List[str]]) -> Tuple[int, np.ndarray]:
-    """
-    Generate audio from text
-
-    Params:
-        conversation: conversation history with the chatbot
-    Returns:
-        Sample rate and generated audio in form of numpy array
-    """
-    prompt = conversation[-1][-1]
-    # generate audio for the last prompt (bot's response)
-    return SAMPLE_RATE, tts_model.generate_audio(prompt)
-
-
-def transcribe(audio: Tuple[int, np.ndarray], conversation: List[List[str]]) -> List[List[str]]:
-    """
-    Transcribe audio to text
-
-    Params:
-        audio: audio to transcribe text from
-        conversation: conversation history with the chatbot
-    Returns:
-        User prompt as a text
-    """
-    sample_rate, audio = audio
-    # the whisper model requires 16000Hz, not 44100Hz
-    audio = librosa.resample(audio.astype(np.float32), orig_sr=sample_rate, target_sr=AUDIO_WIDGET_SAMPLE_RATE).astype(np.int16)
-
-    # get input features from the audio
-    input_features = asr_processor(audio, sampling_rate=AUDIO_WIDGET_SAMPLE_RATE, return_tensors="pt").input_features
-    # get output
-    predicted_ids = asr_model.generate(input_features)
-    # decode output to text
-    transcription = asr_processor.batch_decode(predicted_ids, skip_special_tokens=True)[0]
-
-    # add the text to the conversation
-    conversation.append([transcription, None])
-    return conversation
-
-
-def create_UI(initial_message: str) -> gr.Blocks:
-    """
-    Create web user interface
-
-    Params:
-        initial_message: message to start with
-    Returns:
-        Demo UI
-    """
-    with gr.Blocks(title="Talk to Adrishuo - a conversational voice agent") as demo:
-        gr.Markdown("""
-        # Talk to Adrishuo - a conversational voice agent
-        
-        Instructions for use:
-        - record your question/comment using the first audio widget ("Your voice input")
-        - wait for the chatbot to response ("Chatbot")
-        - wait for the output voice in the last audio widget ("Chatbot voice output")
-        """)
-        with gr.Row():
-            # user's input
-            input_audio_ui = gr.Audio(sources=["microphone"], scale=5, label="Your voice input")
-            # submit button
-            submit_audio_btn = gr.Button("Submit", variant="primary", scale=1)
-
-        # chatbot
-        chatbot_ui = gr.Chatbot(value=[[None, initial_message]], label="Chatbot")
-        # chatbot's audio response
-        output_audio_ui = gr.Audio(autoplay=True, interactive=False, label="Chatbot voice output")
-
-        # events
-        submit_audio_btn.click(transcribe, inputs=[input_audio_ui, chatbot_ui], outputs=chatbot_ui)\
-            .then(chat, chatbot_ui, chatbot_ui)\
-            .then(synthesize, chatbot_ui, output_audio_ui)
-    return demo
-
-
-def run(asr_model_dir: Path, chat_model_dir: Path, tts_model_dir: Path, speaker_type: str, public_interface: bool = False) -> None:
+def run(chat_model_dir: Path, public_interface: bool = False) -> None:
     """
     Run the assistant application
 
     Params
-        asr_model_dir: dir with the automatic speech recognition model
         chat_model_dir: dir with the chat model
-        tts_model_dir: dir with the text-to-speech model
-        speaker_type: type of voice: male or female
         public_interface: whether UI should be available publicly
     """
-    # load whisper model
-    load_asr_model(asr_model_dir)
     # load chat model
     load_chat_model(chat_model_dir)
-    # load bark model
-    load_tts_model(tts_model_dir, speaker_type)
 
-    # get initial greeting
-    history = [[None, None]]
-    history = chat(history)
-    initial_message = history[0][1]
+    history = []
+    initial_prompt = "Please introduce yourself and greet the customer"
+    initial_message = chat(initial_prompt, history)
 
-    # create user interface
-    demo = create_UI(initial_message)
-    # launch demo
-    demo.launch(share=public_interface)
+    chat_ui = gr.Chatbot(value=[(None, initial_message)])
+    chatbot_ui = gr.ChatInterface(fn=chat, chatbot=chat_ui, title="Talk to Adrishuo - a conversational voice agent")
+
+    chatbot_ui.launch(share=public_interface)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--asr_model_dir', type=str, default="model/distil-large-v2-FP16", help="Path to the automatic speech recognition model directory")
-    parser.add_argument('--chat_model_dir', type=str, default="model/llama2-7B-INT8", help="Path to the chat model directory")
-    parser.add_argument('--tts_model_dir', type=str, default="model/TTS-bark-small-FP16", help="Path to the text-to-speech model directory")
-    parser.add_argument('--tts_speaker_type', type=str, default="male", choices=["male", "female"], help="The speaker's voice type")
+    parser.add_argument('--chat_model_dir', type=str, default="model/llama2-7B", help="Path to the chat model directory")
     parser.add_argument('--public_interface', default=False, action="store_true", help="Whether interface should be available publicly")
 
     args = parser.parse_args()
-    run(Path(args.asr_model_dir), Path(args.chat_model_dir), Path(args.tts_model_dir), args.tts_speaker_type, args.public_interface)
+    run(Path(args.chat_model_dir), args.public_interface)
