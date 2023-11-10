48082010a367ce31e1c313bb34c87b4edc048a8a
Adrian Boguszewski <adrian.boguszewski@intel.com>
2023-11-10 18:14:16 +0000

    Free resources after conversion, restore use_small parameter

--- recipes/conversational_voice_agent/convert_and_optimize_TTS.py
+++ recipes/conversational_voice_agent/convert_and_optimize_TTS.py
@@ -51,7 +51,7 @@ class FineModel(nn.Module):
 
 
 # Function to download and convert the text encoder model
-def download_and_convert_text_encoder(models_dir: Path) -> None:
+def download_and_convert_text_encoder(models_dir: Path, use_small: bool) -> None:
     """
     Downloads and converts the text encoder model to OpenVINO format.
     
@@ -61,6 +61,7 @@ def download_and_convert_text_encoder(models_dir: Path) -> None:
 
     Parameters:
         models_dir (Path): The directory where the OpenVINO model files will be saved.
+        use_small (bool): If set to True, the smaller variant of the model is used.
     """
     text_model_dir = models_dir / "text_encoder"
     text_model_dir.mkdir(exist_ok=True)
@@ -68,24 +69,27 @@ def download_and_convert_text_encoder(models_dir: Path) -> None:
     text_encoder_path0 = text_model_dir / "bark_text_encoder_0.xml"
 
     if not text_encoder_path0.exists() or not text_encoder_path1.exists():
-        text_encoder = load_model(
-            model_type="text", use_gpu=False, force_reload=False
-        )
+        text_encoder = load_model(model_type="text", use_gpu=False, force_reload=False, use_small=use_small)
         text_encoder_model = TextEncoderModel(text_encoder["model"])
-        ov_model = ov.convert_model(
+        ov_model_1 = ov.convert_model(
             text_encoder_model, example_input=torch.ones((1, 513), dtype=torch.int64)
         )
-        ov.save_model(ov_model, text_encoder_path0)
+        ov.save_model(ov_model_1, text_encoder_path0)
         logits, kv_cache = text_encoder_model(torch.ones((1, 513), dtype=torch.int64))
-        ov_model = ov.convert_model(
+        ov_model_2 = ov.convert_model(
             text_encoder_model,
             example_input=(torch.ones((1, 1), dtype=torch.int64), kv_cache),
         )
-        ov.save_model(ov_model, text_encoder_path1)
-  
+        ov.save_model(ov_model_2, text_encoder_path1)
+
+        # free resources
+        del text_encoder
+        del ov_model_1
+        del ov_model_2
+
 
 # Function to download and convert the coarse encoder model
-def download_and_convert_coarse_encoder(models_dir: Path) -> None:
+def download_and_convert_coarse_encoder(models_dir: Path, use_small: bool) -> None:
     """
     Downloads and converts the coarse encoder model to OpenVINO format.
 
@@ -95,15 +99,14 @@ def download_and_convert_coarse_encoder(models_dir: Path) -> None:
 
     Parameters:
         models_dir (Path): The directory where the OpenVINO model files will be saved.
+        use_small (bool): If set to True, the smaller variant of the model is used.
     """
     coarse_model_dir = models_dir / "coarse_model"
     coarse_model_dir.mkdir(exist_ok=True)
     coarse_encoder_path = coarse_model_dir / "bark_coarse_encoder.xml"
     
     if not coarse_encoder_path.exists():
-        coarse_model = load_model(
-            model_type="coarse", use_gpu=False, force_reload=False
-        )
+        coarse_model = load_model(model_type="coarse", use_gpu=False, force_reload=False, use_small=use_small)
         coarse_encoder_exportable = CoarseEncoderModel(coarse_model)
         logits, kv_cache = coarse_encoder_exportable(torch.ones((1, 886), dtype=torch.int64))
         ov_model = ov.convert_model(
@@ -112,9 +115,13 @@ def download_and_convert_coarse_encoder(models_dir: Path) -> None:
         )
         ov.save_model(ov_model, coarse_encoder_path)
 
+        # free resources
+        del coarse_model
+        del ov_model
+
 
 # Function to download and convert the fine model
-def download_and_convert_fine_model(models_dir: Path) -> None:
+def download_and_convert_fine_model(models_dir: Path, use_small: bool) -> None:
     """
     Downloads and converts the fine model to OpenVINO format.
 
@@ -124,12 +131,13 @@ def download_and_convert_fine_model(models_dir: Path) -> None:
 
     Parameters:
         models_dir (Path): The directory where the OpenVINO model files will be saved.
+        use_small (bool): If set to True, the smaller variant of the model is used.
     """
     fine_model_dir = models_dir / "fine_model"
     fine_model_dir.mkdir(exist_ok=True)
     fine_feature_extractor_path = fine_model_dir / "bark_fine_feature_extractor.xml"
     if not fine_feature_extractor_path.exists():
-        fine_model = load_model(model_type="fine", use_gpu=False, force_reload=False)
+        fine_model = load_model(model_type="fine", use_gpu=False, force_reload=False, use_small=use_small)
         fine_feature_extractor = FineModel(fine_model)
         feature_extractor_out = fine_feature_extractor(
             3, torch.zeros((1, 1024, 8), dtype=torch.int32)
@@ -150,6 +158,10 @@ def download_and_convert_fine_model(models_dir: Path) -> None:
                 lm_head_model,
                 fine_model_dir / f"bark_fine_lm_{i}.xml",
             )
+            # free resources
+            del lm_head_model
+
+        del fine_model
 
 
 def convert_bark(use_small: bool) -> None:
@@ -165,9 +177,9 @@ def convert_bark(use_small: bool) -> None:
     models_dir = Path(f"model/TTS_bark{model_suffix}")
     models_dir.mkdir(parents=True, exist_ok=True)
     
-    download_and_convert_text_encoder(models_dir)
-    download_and_convert_coarse_encoder(models_dir)
-    download_and_convert_fine_model(models_dir)
+    download_and_convert_text_encoder(models_dir, use_small)
+    download_and_convert_coarse_encoder(models_dir, use_small)
+    download_and_convert_fine_model(models_dir, use_small)
     
     print("All models have been downloaded and converted successfully.")
 
