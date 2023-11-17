330589af5ead5747c8110da44b4af555fe81a203
Ekaterina Aidova <ekaterina.aidova@intel.com>
2023-11-17 13:22:00 +0400

    Fix chatglm2 quantization (#1473)
    * Fix chatglm2 quantization
    
    * Update 254-llm-chatbot.ipynb

--- notebooks/254-llm-chatbot/config.py
+++ notebooks/254-llm-chatbot/config.py
@@ -11,16 +11,18 @@ If a question does not make any sense, or is not factually coherent, explain why
 
 
 def red_pijama_partial_text_processor(partial_text, new_text):
-    if new_text == '<':
+    if new_text == "<":
         return partial_text
-    
+
     partial_text += new_text
-    return partial_text.split('<bot>:')[-1]
+    return partial_text.split("<bot>:")[-1]
+
 
 def llama_partial_text_processor(partial_text, new_text):
     new_text = new_text.replace("[INST]", "").replace("[/INST]", "")
     partial_text += new_text
     return partial_text
+
 
 def chatglm_partial_text_processor(partial_text, new_text):
     new_text = new_text.strip()
@@ -28,11 +30,57 @@ def chatglm_partial_text_processor(partial_text, new_text):
     partial_text += new_text
     return partial_text
 
+
 SUPPORTED_MODELS = {
-  "red-pajama-3b-chat" : {"model_id": "togethercomputer/RedPajama-INCITE-Chat-3B-v1", "remote": False, "start_message": "", "history_template": "\n<human>:{user}\n<bot>:{assistant}", "stop_tokens": [29, 0], "partial_text_processor": red_pijama_partial_text_processor, "current_message_template": "\n<human>:{user}\n<bot>:{assistant}"},
-  "llama-2-chat-7b": {"model_id": "meta-llama/Llama-2-7b-chat-hf", "remote": False, "start_message": f"<s>[INST] <<SYS>>\n{DEFAULT_SYSTEM_PROMPT }\n<</SYS>>\n\n", "history_template": "{user}[/INST]{assistant}</s><s>[INST]", "current_message_template": "{user} [/INST]{assistant}","tokenizer_kwargs": {"add_special_tokens":False}, "partial_text_processor": llama_partial_text_processor, "revision": "5514c85fedd6c4fc0fc66fa533bc157de520da73"},
-  "mpt-7b-chat": {"model_id": "mosaicml/mpt-7b-chat", "remote": True, "start_message": f"<|im_start|>system\n {DEFAULT_SYSTEM_PROMPT }<|im_end|>", "history_template": "<|im_start|>user\n{user}<im_end><|im_start|>assistant\n{assistant}<|im_end|>", "current_message_template": "\"<|im_start|>user\n{user}<im_end><|im_start|>assistant\n{assistant}", "stop_tokens": ["<|im_end|>", "<|endoftext|>"]},
- "qwen-7b-chat": {"model_id": "Qwen/Qwen-7B-Chat", "remote": True, "start_message": f"<|im_start|>system\n {DEFAULT_SYSTEM_PROMPT }<|im_end|>", "history_template": "<|im_start|>user\n{user}<im_end><|im_start|>assistant\n{assistant}<|im_end|>", "current_message_template": "\"<|im_start|>user\n{user}<im_end><|im_start|>assistant\n{assistant}", "stop_tokens": ["<|im_end|>", "<|endoftext|>"]},
-  "chatglm2-6b": {"model_id": "THUDM/chatglm2-6b", "remote": True, "start_message": f"{DEFAULT_SYSTEM_PROMPT }\n\n", "history_template": "[Round{num}\n\n问：{user}\n\n答：{assistant}\n\n", "partial_text_processor": chatglm_partial_text_processor, "current_message_template": "[Round{num}\n\n问：{user}\n\n答：{assistant}", "stop_tokens": ["</s>"]},
-"zephyr-7b-beta": {"model_id": "HuggingFaceH4/zephyr-7b-beta", "remote": False, "start_message":f'<|system|>\n{DEFAULT_SYSTEM_PROMPT}</s>\n', "history_template": "<|user|>\n{user}</s> \n<|assistant|>\n{assistant}</s> \n",  "current_message_template": "<|user|>\n{user}</s> \n<|assistant|>\n{assistant}"}
+    "red-pajama-3b-chat": {
+        "model_id": "togethercomputer/RedPajama-INCITE-Chat-3B-v1",
+        "remote": False,
+        "start_message": "",
+        "history_template": "\n<human>:{user}\n<bot>:{assistant}",
+        "stop_tokens": [29, 0],
+        "partial_text_processor": red_pijama_partial_text_processor,
+        "current_message_template": "\n<human>:{user}\n<bot>:{assistant}",
+    },
+    "llama-2-chat-7b": {
+        "model_id": "meta-llama/Llama-2-7b-chat-hf",
+        "remote": False,
+        "start_message": f"<s>[INST] <<SYS>>\n{DEFAULT_SYSTEM_PROMPT }\n<</SYS>>\n\n",
+        "history_template": "{user}[/INST]{assistant}</s><s>[INST]",
+        "current_message_template": "{user} [/INST]{assistant}",
+        "tokenizer_kwargs": {"add_special_tokens": False},
+        "partial_text_processor": llama_partial_text_processor,
+        "revision": "5514c85fedd6c4fc0fc66fa533bc157de520da73",
+    },
+    "mpt-7b-chat": {
+        "model_id": "mosaicml/mpt-7b-chat",
+        "remote": True,
+        "start_message": f"<|im_start|>system\n {DEFAULT_SYSTEM_PROMPT }<|im_end|>",
+        "history_template": "<|im_start|>user\n{user}<im_end><|im_start|>assistant\n{assistant}<|im_end|>",
+        "current_message_template": '"<|im_start|>user\n{user}<im_end><|im_start|>assistant\n{assistant}',
+        "stop_tokens": ["<|im_end|>", "<|endoftext|>"],
+    },
+    "qwen-7b-chat": {
+        "model_id": "Qwen/Qwen-7B-Chat",
+        "remote": True,
+        "start_message": f"<|im_start|>system\n {DEFAULT_SYSTEM_PROMPT }<|im_end|>",
+        "history_template": "<|im_start|>user\n{user}<im_end><|im_start|>assistant\n{assistant}<|im_end|>",
+        "current_message_template": '"<|im_start|>user\n{user}<im_end><|im_start|>assistant\n{assistant}',
+        "stop_tokens": ["<|im_end|>", "<|endoftext|>"],
+    },
+    "chatglm2-6b": {
+        "model_id": "THUDM/chatglm2-6b",
+        "remote": True,
+        "start_message": f"{DEFAULT_SYSTEM_PROMPT }\n\n",
+        "history_template": "[Round{num}\n\n问：{user}\n\n答：{assistant}\n\n",
+        "partial_text_processor": chatglm_partial_text_processor,
+        "current_message_template": "[Round{num}\n\n问：{user}\n\n答：{assistant}",
+        "stop_tokens": ["</s>"],
+    },
+    "zephyr-7b-beta": {
+        "model_id": "HuggingFaceH4/zephyr-7b-beta",
+        "remote": False,
+        "start_message": f"<|system|>\n{DEFAULT_SYSTEM_PROMPT}</s>\n",
+        "history_template": "<|user|>\n{user}</s> \n<|assistant|>\n{assistant}</s> \n",
+        "current_message_template": "<|user|>\n{user}</s> \n<|assistant|>\n{assistant}",
+    },
 }
