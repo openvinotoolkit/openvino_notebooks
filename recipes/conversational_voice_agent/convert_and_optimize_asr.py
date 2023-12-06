d3cfc10e7808fa4b03495b4e5c47f6bc85e9ce6d
Adrian Boguszewski <adrian.boguszewski@intel.com>
2023-12-06 16:21:47 +0100

    Small fixes in the ASR conversion script

--- recipes/conversational_voice_agent/convert_and_optimize_asr.py
+++ recipes/conversational_voice_agent/convert_and_optimize_asr.py
@@ -2,7 +2,9 @@ import argparse
 from pathlib import Path
 
 from optimum.intel.openvino import OVModelForSpeechSeq2Seq
-from transformers import AutoProcessor, AutoModelForSpeechSeq2Seq
+from transformers import AutoProcessor
+
+MODEL_NAME = "distil-whisper/distil-large-v2"
 
 
 def convert_asr_model(model_dir: Path) -> Path:
@@ -12,28 +14,22 @@ def convert_asr_model(model_dir: Path) -> Path:
     Params:
         model_dir: dir to export model
     Returns:
-       Path to exported model
+       Path to exported model dir
     """
-     
-    model_name = "distil-whisper/distil-large-v2"
-    output_dir = model_dir / model_name.rsplit ("/")[-1]
+    output_dir = model_dir / (MODEL_NAME.rsplit ("/")[-1] + "-FP16")
 
-    
     # load model and convert it to OpenVINO
-    model = OVModelForSpeechSeq2Seq.from_pretrained(model_name, export=True, compile=False)
+    model = OVModelForSpeechSeq2Seq.from_pretrained(MODEL_NAME, export=True, compile=False)
     # change precision to FP16
     model.half()
     # save model to disk
-    output_dir = output_dir.with_name(output_dir.name + "-FP16")
     model.save_pretrained(output_dir)
 
-
     # export also processor
-    asr_processor = AutoProcessor.from_pretrained(model_name)
+    asr_processor = AutoProcessor.from_pretrained(MODEL_NAME)
     asr_processor.save_pretrained(output_dir)
 
-
-    return Path(output_dir) / "openvino_model.xml"
+    return Path(output_dir)
 
 
 if __name__ == "__main__":
