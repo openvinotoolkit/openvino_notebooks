cc43d871ddbb8229bff36391a7eb3a942934fbd0
yatarkan <yaroslav.tarkan@intel.com>
2023-12-11 19:59:03 +0400

    Add button component

--- selector/vite.config.ts
+++ selector/vite.config.ts
@@ -1,10 +1,11 @@
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
