dcf4a269a46c8a73ae5fa755c70ccf0126fd165a
AnishaUdayakumar <anisha.udayakumar@intel.com>
2024-01-12 08:33:33 -0800

    Updates for speechT5

---
+++ recipes/conversational_voice_agent/load_tts.py
@@ -0,0 +1,26 @@
+import nltk
+from nltk.tokenize import word_tokenize
+
+# Attempt to download the 'punkt' tokenizer
+try:
+    nltk.data.find('tokenizers/punkt')
+except LookupError:
+    nltk.download('punkt')
+
+
+from transformers import SpeechT5Processor, SpeechT5ForTextToSpeech, SpeechT5HifiGan
+
+def download_models():
+    print("Downloading SpeechT5Processor...")
+    SpeechT5Processor.from_pretrained("microsoft/speecht5_tts")
+
+    print("Downloading SpeechT5ForTextToSpeech...")
+    SpeechT5ForTextToSpeech.from_pretrained("microsoft/speecht5_tts")
+
+    print("Downloading SpeechT5HifiGan...")
+    SpeechT5HifiGan.from_pretrained("microsoft/speecht5_hifigan")
+
+    print("All models downloaded successfully.")
+
+if __name__ == "__main__":
+    download_models()
