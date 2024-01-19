0adeee14e64794b6937af9c464f448838fcedb40
roszczyk <mikolaj.roszczyk@intel.com>
2024-01-19 11:26:06 +0100

    Downloading model via utils.download_file; to do: organizing, convertion

---
+++ notebooks/204-segmenter-semantic-segmentation/segm_fix_lib.py
@@ -0,0 +1,81 @@
+from pathlib import Path
+import yaml
+import torch
+import os
+import timm
+
+from timm.models.helpers import load_pretrained, load_custom_pretrained
+from timm.models.vision_transformer import default_cfgs
+
+from segmenter.segm.model.vit import VisionTransformer
+from segmenter.segm.model.utils import checkpoint_filter_fn
+from segmenter.segm.model.factory import create_decoder
+from segmenter.segm.model.segmenter import Segmenter
+import segmenter.segm.utils.torch as ptu
+
+def create_vit(model_cfg):
+    model_cfg = model_cfg.copy()
+    backbone = model_cfg.pop("backbone")
+
+    normalization = model_cfg.pop("normalization")
+    model_cfg["n_cls"] = 1000
+    mlp_expansion_ratio = 4
+    model_cfg["d_ff"] = mlp_expansion_ratio * model_cfg["d_model"]
+
+    if backbone in default_cfgs:
+        default_cfg = default_cfgs[backbone]
+    else:
+        default_cfg = dict(
+            pretrained=False,
+            num_classes=1000,
+            drop_rate=0.0,
+            drop_path_rate=0.0,
+            drop_block_rate=None,
+        )
+    
+    print(default_cfg)
+
+    default_cfg["input_size"] = (
+        3,
+        model_cfg["image_size"][0],
+        model_cfg["image_size"][1],
+    )
+    model = VisionTransformer(**model_cfg)
+    if backbone == "vit_base_patch8_384":
+        path = os.path.expandvars("$TORCH_HOME/hub/checkpoints/vit_base_patch8_384.pth")
+        state_dict = torch.load(path, map_location="cpu")
+        filtered_dict = checkpoint_filter_fn(state_dict, model)
+        model.load_state_dict(filtered_dict, strict=True)
+    elif "deit" in backbone:
+        load_pretrained(model, default_cfg, filter_fn=checkpoint_filter_fn)
+    else:
+        load_custom_pretrained(model, default_cfg)
+
+    return model
+
+
+def create_segmenter(model_cfg):
+    model_cfg = model_cfg.copy()
+    decoder_cfg = model_cfg.pop("decoder")
+    decoder_cfg["n_cls"] = model_cfg["n_cls"]
+
+    encoder = create_vit(model_cfg)
+    decoder = create_decoder(encoder, decoder_cfg)
+    model = Segmenter(encoder, decoder, n_cls=model_cfg["n_cls"])
+
+    return model
+
+
+def load_model(model_path):
+    variant_path = Path(model_path).parent / "variant.yml"
+    with open(variant_path, "r") as f:
+        variant = yaml.load(f, Loader=yaml.FullLoader)
+    net_kwargs = variant["net_kwargs"]
+
+    model = create_segmenter(net_kwargs)
+    data = torch.load(model_path, map_location=ptu.device)
+    checkpoint = data["model"]
+
+    model.load_state_dict(checkpoint, strict=True)
+
+    return model, variant
