b95ce9eeed1d906dc1beaa64a3f017ae7757e943
Nikita Savelyev <nikita.savelyev@intel.com>
2024-02-02 13:33:38 +0100

    WIP

--- notebooks/236-stable-diffusion-v2/quantize.py
+++ notebooks/236-stable-diffusion-v2/quantize.py
@@ -7,36 +7,37 @@ import nncf
 import numpy as np
 import openvino as ov
 from optimum.intel.openvino import OVStableDiffusionPipeline
-from diffusers import StableDiffusionPipeline # DPMSolverMultistepScheduler
+from diffusers import StableDiffusionPipeline, DPMSolverMultistepScheduler, LMSDiscreteScheduler
 from transformers import set_seed
 
 RANDOM_TEST_DATA = [
     "a black and brown dog standing outside a door.",
-    "a person on a motorcycle makes a turn on the track.",
-    "inflatable boats sit on the arizona river, and on the bank",
-    "a white cat sitting under a white umbrella",
-    "black bear standing in a field of grass under a tree.",
-    "a train that is parked on tracks and has graffiti writing on it, with a mountain range in the background.",
-    "a cake inside of a pan sitting in an oven.",
-    "a table with paper plates and flowers in a home",
-    "a photo of an astronaut riding a horse on mars",
-    "Super cute fluffy cat warrior in armor, photorealistic, 4K, ultra detailed, vray rendering, unreal engine",
-    "ultra close color photo portrait of rainbow owl with deer horns in the woods",
+    # "a person on a motorcycle makes a turn on the track.",
+    # "inflatable boats sit on the arizona river, and on the bank",
+    # "a white cat sitting under a white umbrella",
+    # "black bear standing in a field of grass under a tree.",
+    # "a train that is parked on tracks and has graffiti writing on it, with a mountain range in the background.",
+    # "a cake inside of a pan sitting in an oven.",
+    # "a table with paper plates and flowers in a home",
+    # "a photo of an astronaut riding a horse on mars",
+    # "Super cute fluffy cat warrior in armor, photorealistic, 4K, ultra detailed, vray rendering, unreal engine",
+    # "ultra close color photo portrait of rainbow owl with deer horns in the woods",
 ]
 
 
 def dump_images(model_path, imgs_path):
     set_seed(1)
-    core = ov.Core()
-    model = core.read_model(model_path)
 
     # model_id = "stabilityai/stable-diffusion-2-1"
-    model_id = "/home/ltalamanova/cvs-117617/models/stable-diffusion-2-1"
+    model_id = "/home/nsavel/workspace/openvino_notebooks/notebooks/236-stable-diffusion-v2/ltalamanova/sd2.1"
     pipeline = OVStableDiffusionPipeline.from_pretrained(model_id, compile=False)
-    # help(pipeline.unet)
     # pipeline.scheduler = DPMSolverMultistepScheduler.from_config(pipeline.scheduler.config)
-    pipeline.unet.model = model
+    pipeline.scheduler = LMSDiscreteScheduler.from_config(pipeline.scheduler.config)
+    pipeline.unet.model = ov.Core().read_model(model_path)
+    # pipeline.reshape(batch_size=1, height=512, width=512, num_images_per_prompt=1)
+    # help(pipeline.unet)
     for i, prompt in enumerate(RANDOM_TEST_DATA):
+        # image = pipeline("valley in the Alps at sunset, epic vista, beautiful landscape, 4k, 8k", negative_prompt="frames, borderline, text, charachter, duplicate, error, out of frame, watermark, low quality, ugly, deformed, blur").images[0]
         image = pipeline(prompt).images[0]
         image.save(f"{imgs_path}/{i}.png")
 
@@ -59,6 +60,7 @@ def get_operation_const_op(operation, const_port_id: int):
 
     return constant_node
 
+
 def is_embedding(node) -> bool:
     allowed_types_list = ["f16", "f32", "f64"]
     const_port_id = 0
@@ -69,6 +71,7 @@ def is_embedding(node) -> bool:
             return True
 
     return False
+
 
 def collect_ops_with_weights(model):
     ops_with_weights = []
@@ -84,6 +87,7 @@ def collect_ops_with_weights(model):
 
     return ops_with_weights
 
+
 class CompiledModelDecorator(ov.CompiledModel):
     def __init__(self, compiled_model, prob=0.5):
         super().__init__(compiled_model)
@@ -91,6 +95,7 @@ class CompiledModelDecorator(ov.CompiledModel):
         self.prob = np.clip(prob, 0, 1)
 
     def __call__(self, *args, **kwargs):
+        # print(args[0]["sample"].shape, args[0]["encoder_hidden_states"].shape)
         if np.random.rand() >= self.prob:
             self.data_cache.append(*args)
         return super().__call__(*args, **kwargs)
@@ -119,12 +124,26 @@ def collect_calibration_data(pipeline, subset_size=300):
     return calibration_dataset
 
 
-def create_hybrid_model(model_id, model_path):
-    pipeline = OVStableDiffusionPipeline.from_pretrained(model_id, compile=False)
-    core = ov.Core()
-    unet = core.read_model(model_path)
+def create_hybrid_model(model_dir):
+    pipeline = OVStableDiffusionPipeline.from_pretrained(str(model_dir), compile=False)
+    pipeline.reshape(batch_size=1, height=512, width=512, num_images_per_prompt=1)
+    # core = ov.Core()
+    # unet = core.read_model(model_dir / "unet" / "openvino_model.xml")
+    unet = pipeline.unet.model
     subset_size = 300
-    calibration_data = collect_calibration_data(pipeline, subset_size)
+
+    unet_ignored_scope = collect_ops_with_weights(unet)
+    print(f"Found {len(unet_ignored_scope)} nodes with compressed weights.")
+
+    calibration_cache_path = model_dir / "calibration_data_512.pkl"
+    if calibration_cache_path.exists():
+        with open(calibration_cache_path, "rb") as f:
+            calibration_data = pickle.load(f)
+    else:
+        calibration_data = collect_calibration_data(pipeline, subset_size)
+        with open(calibration_cache_path, "wb") as f:
+            pickle.dump(calibration_data, f)
+
     print(f"Prepared {len(calibration_data)} calibration samples")
     quantization_dataset = nncf.Dataset(calibration_data)
     compressed_unet = nncf.compress_weights(unet)
@@ -135,8 +154,6 @@ def create_hybrid_model(model_id, model_path):
     #     ratio=0.8,
     #     sensitivity_metric=nncf.SensitivityMetric.HESSIAN_INPUT_ACTIVATION,
     # )
-    unet_ignored_scope = collect_ops_with_weights(compressed_unet)
-    print(f"Found {len(unet_ignored_scope)} nodes with compressed weights.")
 
     quantized_unet = nncf.quantize(
         model=compressed_unet,
@@ -147,9 +164,16 @@ def create_hybrid_model(model_id, model_path):
         advanced_parameters=nncf.AdvancedQuantizationParameters(smooth_quant_alpha=-1)
     )
 
-    ov.save_model(quantized_unet, '/home/ltalamanova/cvs-117617/models/stable-diffusion-2-1/compressed/int8_asym_hybrid.xml')
+    ov.save_model(quantized_unet, model_dir / "unet" / "openvino_model_int8.xml")
 
-# model_id = "stabilityai/stable-diffusion-2-1"
-model_id = "/home/ltalamanova/cvs-117617/models/stable-diffusion-2-1"
-model_path = "/home/ltalamanova/cvs-117617/models/stable-diffusion-2-1/unet/openvino_model.xml"
-create_hybrid_model(model_id, model_path)
+# pipeline = OVStableDiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-2-1", export=True, compile=False)
+# pipeline.scheduler = DPMSolverMultistepScheduler.from_config(pipeline.scheduler.config)
+# pipeline.save_pretrained("/home/nsavel/workspace/openvino_notebooks/notebooks/236-stable-diffusion-v2/ltalamanova/sd2.1")
+# exit(0)
+
+# model_dir = Path("sd2.1")
+model_dir = Path("/home/nsavel/workspace/openvino_notebooks/notebooks/236-stable-diffusion-v2/ltalamanova/sd2.1")
+# model_dir = Path("/home/nsavel/workspace/openvino_notebooks/notebooks/236-stable-diffusion-v2/helenai/sd2.1-base")
+# create_hybrid_model(model_dir)
+
+dump_images(model_dir / "unet" / "openvino_model_int8.xml", model_dir / "images")
