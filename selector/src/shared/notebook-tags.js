28e04d48f532bf151db41d4c0974130b05615691
Alexander Suvorov <alexander.suvorov@intel.com>
2024-02-12 16:14:03 +0100

    Fix formatting

--- selector/src/shared/notebook-tags.js
+++ selector/src/shared/notebook-tags.js
@@ -31,7 +31,7 @@ export const TASKS = /** @type {const} */ ({
     SUPER_RESOLUTION: 'Super Resolution',
     STYLE_TRANSFER: 'Style Transfer',
     POSE_ESTIMATION: 'Pose Estimation',
-    ZERO_SHOT_IMAGE_CLASSIFICATION: 'Zero-Shot Image Classification',,
+    ZERO_SHOT_IMAGE_CLASSIFICATION: 'Zero-Shot Image Classification',
     TEXT_DETECTION: 'Text Detection',
   },
   NLP: {
