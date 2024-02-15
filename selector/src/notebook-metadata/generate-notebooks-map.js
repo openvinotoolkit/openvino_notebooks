52dda7399051b8e1569db8aa86d1a85b62c0b5b6
yatarkan <yaroslav.tarkan@intel.com>
2024-02-15 21:26:29 +0400

    Fix generate notebooks map plugin imports with extra api requests

--- selector/src/notebook-metadata/generate-notebooks-map.js
+++ selector/src/notebook-metadata/generate-notebooks-map.js
@@ -3,7 +3,6 @@
 import { existsSync, mkdirSync, readFileSync, writeFileSync } from 'fs';
 import { join, resolve } from 'path';
 
-import { NotebookMetadataHandler } from './notebook-metadata-handler.js';
 import { NotebookMetadataValidationError } from './notebook-metadata-validator.js';
 
 export const NOTEBOOKS_MAP_FILE_NAME = 'notebooks-metadata-map.json';
@@ -12,15 +11,17 @@ export const NOTEBOOKS_MAP_FILE_NAME = 'notebooks-metadata-map.json';
  *
  * @param {string} path
  * @throws {NotebookMetadataValidationError}
- * @returns {void}
+ * @returns {Promise<void>}
  */
-function generateNotebooksMapFile(path) {
+async function generateNotebooksMapFile(path) {
   /** @typedef {import("./notebook-metadata-collector.js").INotebookMetadata} INotebookMetadata */
 
   /** @type {Record<string, INotebookMetadata>} */
   const notebooksMetadataMap = {};
 
   console.info(`Creating notebooks map file...`);
+
+  const { NotebookMetadataHandler } = await import('./notebook-metadata-handler.js');
 
   const notebooksPaths = NotebookMetadataHandler.getNotebooksPaths();
 
@@ -61,19 +62,19 @@ export const generateNotebooksMapFilePlugin = () => {
       config = resolvedConfig;
       distPath = resolve(config.root, config.build.outDir);
     },
-    closeBundle() {
+    async closeBundle() {
       if (config.command === 'build') {
-        generateNotebooksMapFile(distPath);
+        await generateNotebooksMapFile(distPath);
       }
     },
-    configureServer(devServer) {
+    async configureServer(devServer) {
       const notebooksMapFileExists = existsSync(join(distPath, NOTEBOOKS_MAP_FILE_NAME));
       if (notebooksMapFileExists) {
         console.info(
           `"${NOTEBOOKS_MAP_FILE_NAME}" file already exists and is served from "${distPath}" dist directory.`
         );
       } else {
-        generateNotebooksMapFile(distPath);
+        await generateNotebooksMapFile(distPath);
       }
 
       devServer.middlewares.use(`${config.base}${NOTEBOOKS_MAP_FILE_NAME}`, (_, res) => {
