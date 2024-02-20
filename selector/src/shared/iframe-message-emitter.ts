50a52387d8914b0f69c3b937de4799f0bb5e69b9
yatarkan <yaroslav.tarkan@intel.com>
2024-02-20 19:05:45 +0400

    Fix scroll event for embedded mode

--- selector/src/shared/iframe-message-emitter.ts
+++ selector/src/shared/iframe-message-emitter.ts
@@ -1,3 +1,5 @@
+import { isEmbedded } from './iframe-detector';
+
 export interface IResizeMessage {
   type: 'resize';
   height: number;
@@ -11,7 +13,7 @@ export const sendScrollMessage = (): void => {
   const message: IScrollMessage = {
     type: 'scroll',
   };
-  window.parent.postMessage(message);
+  window.parent.postMessage(message, '*');
 };
 
 const report = () => {
@@ -23,3 +25,7 @@ const report = () => {
 };
 
 new ResizeObserver(report).observe(document.body);
+
+if (isEmbedded) {
+  document.body.classList.add('embedded');
+}
