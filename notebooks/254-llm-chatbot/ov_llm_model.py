7b51a15539d23a2a92fced7e0a93f716b6e3f1f7
Ethan Yang <ethan.yang@intel.com>
2024-03-07 05:44:02 -0800

    add internlm2 in LLM notebook (#1788)
    including:
    
    1. add internLM2-1.8b, qwen1.5-1.8b, qwen1.5-4b support
    2. add language filter when selecting the models
    3. update MiniCPM's config files with Chinese prompt template
    4. Fix Baichuan2 tokenization issue
    5. rebase QWEN support for further marketing collaboration

--- notebooks/254-llm-chatbot/ov_llm_model.py
+++ notebooks/254-llm-chatbot/ov_llm_model.py
@@ -204,79 +204,6 @@ class OVMPTModel(OVModelForCausalLM):
         )
 
 
-class OVBAICHUANModel(OVModelForCausalLM):
-    """
-    Optimum intel compatible model wrapper for QWEN
-    """
-
-    def __init__(
-        self,
-        model: "Model",
-        config: "PretrainedConfig" = None,
-        device: str = "CPU",
-        dynamic_shapes: bool = True,
-        ov_config: Optional[Dict[str, str]] = None,
-        model_save_dir: Optional[Union[str, Path]] = None,
-        **kwargs,
-    ):
-        NormalizedConfigManager._conf["baichuan"] = NormalizedTextConfig.with_args(
-            num_layers="num_hidden_layers",
-            num_attention_heads="num_attention_heads",
-            hidden_size="hidden_size",
-        )
-        super().__init__(
-            model, config, device, dynamic_shapes, ov_config, model_save_dir, **kwargs
-        )
-
-    def _reshape(self, model: "Model", *args, **kwargs):
-        shapes = {}
-        for inputs in model.inputs:
-            shapes[inputs] = inputs.get_partial_shape()
-            if inputs.get_any_name().startswith('beam_idx'):
-                continue
-            shapes[inputs][1] = -1
-        model.reshape(shapes)
-        return model
-
-    @classmethod
-    def _from_pretrained(
-        cls,
-        model_id: Union[str, Path],
-        config: PretrainedConfig,
-        use_auth_token: Optional[Union[bool, str, None]] = None,
-        revision: Optional[Union[str, None]] = None,
-        force_download: bool = False,
-        cache_dir: Optional[str] = None,
-        file_name: Optional[str] = None,
-        subfolder: str = "",
-        from_onnx: bool = False,
-        local_files_only: bool = False,
-        load_in_8bit: bool = False,
-        **kwargs,
-    ):
-        model_path = Path(model_id)
-        default_file_name = OV_XML_FILE_NAME
-        file_name = file_name or default_file_name
-
-        model_cache_path = cls._cached_file(
-            model_path=model_path,
-            use_auth_token=use_auth_token,
-            revision=revision,
-            force_download=force_download,
-            cache_dir=cache_dir,
-            file_name=file_name,
-            subfolder=subfolder,
-            local_files_only=local_files_only,
-        )
-
-        model = cls.load_model(model_cache_path, load_in_8bit=load_in_8bit)
-        init_cls = OVBAICHUANModel
-
-        return init_cls(
-            model=model, config=config, model_save_dir=model_cache_path.parent, **kwargs
-        )
-
-
 class OVCHATGLMModel(OVModelForCausalLM):
     """
     Optimum intel compatible model wrapper for CHATGLM2
@@ -354,11 +281,158 @@ class OVCHATGLMModel(OVModelForCausalLM):
         return init_cls(
             model=model, config=config, model_save_dir=model_cache_path.parent, **kwargs
         )
+        
+        
+class OVQWENModel(OVModelForCausalLM):
+    """
+    Optimum intel compatible model wrapper for QWEN
+    """
+    def __init__(
+        self,
+        model: "Model",
+        config: "PretrainedConfig" = None,
+        device: str = "CPU",
+        dynamic_shapes: bool = True,
+        ov_config: Optional[Dict[str, str]] = None,
+        model_save_dir: Optional[Union[str, Path]] = None,
+        **kwargs,
+    ):
+        NormalizedConfigManager._conf["qwen"] = NormalizedTextConfig.with_args(
+            num_layers="num_hidden_layers",
+            num_attention_heads="num_attention_heads",
+            hidden_size="hidden_size",
+        )
+        super().__init__(
+            model, config, device, dynamic_shapes, ov_config, model_save_dir, **kwargs
+        )
+        
+    def _reshape(self, model: "Model", *args, **kwargs):
+        shapes = {}
+        for inputs in model.inputs:
+            shapes[inputs] = inputs.get_partial_shape()
+            if inputs.get_any_name().startswith('beam_idx'):
+                continue
+            shapes[inputs][1] = -1
+        model.reshape(shapes)
+        return model
+
+    def forward(
+        self,
+        input_ids: torch.LongTensor,
+        past_key_values: Optional[Tuple[Tuple[torch.FloatTensor]]] = None,
+        attention_mask: Optional[torch.LongTensor] = None,
+        token_type_ids: Optional[torch.LongTensor] = None,
+        **kwargs,
+    ) -> CausalLMOutputWithPast:
+        self.compile()
+
+        if self.use_cache and past_key_values is not None:
+            input_ids = input_ids[:, -1:]
+
+        batch_size = input_ids.shape[0]
+
+        inputs = {}
+        past_len = 0
+        if not self.stateful:
+            if past_key_values is not None:
+                past_len = past_key_values[0][1].shape[-2]
+                if self._pkv_precision == Type.bf16:
+                    # numpy does not support bf16, pretending f16, should change to bf16
+                    past_key_values = tuple(
+                        Tensor(past_key_value, past_key_value.shape, Type.bf16)
+                        for pkv_per_layer in past_key_values
+                        for past_key_value in pkv_per_layer
+                    )
+                else:
+                    # Flatten the past_key_values
+                    past_key_values = tuple(
+                        past_key_value for pkv_per_layer in past_key_values for past_key_value in pkv_per_layer
+                    )
+                
+
+                # Add the past_key_values to the decoder inputs
+                inputs = dict(zip(self.key_value_input_names, past_key_values))
+
+            # Create empty past_key_values for decoder_with_past first generation step
+            elif self.use_cache:
+                for input_name in self.key_value_input_names:
+                    model_inputs = self.model.input(input_name)
+                    shape = model_inputs.get_partial_shape()
+                    if self.config.model_type == 'chatglm':
+                        shape[0] = 0
+                        shape[1] = batch_size
+                    else:
+                        shape[0] = batch_size
+                        if shape[2].is_dynamic:
+                            shape[2] = 0
+                        elif shape.rank.get_length() == 4 and shape[3].is_dynamic:
+                            shape[3] = 0
+                        else:
+                            shape[1] = 0
+                    inputs[input_name] = Tensor(model_inputs.get_element_type(), shape.get_shape())
+        else:
+            # past_key_values are not used explicitly, instead they are handled inside the model
+            if past_key_values is None:
+                # Need a marker to differentiate the first generate iteration from the others in
+                # the first condition at the function beginning above.
+                # It should be something that is not None and it should be True when converted to Boolean.
+                past_key_values = ((),)
+                # This is the first iteration in a sequence, reset all states
+                for state in self.request.query_state():
+                    state.reset()
+                # Set initial value for the next beam_idx input that will be used at the current iteration
+                # and will be optionally updated by _reorder_cache at the next iterations if beam_search is used
+                self.next_beam_idx = np.array(range(batch_size), dtype=int)
+
+        inputs["input_ids"] = np.array(input_ids)
+        # Add the attention_mask inputs when needed
+        if "attention_mask" in self.input_names or "token_type_ids" in self.input_names:
+            if attention_mask is not None:
+                attention_mask = np.array(attention_mask)
+            else:
+                attention_mask = np.ones(
+                    (input_ids.shape[0], input_ids.shape[1] + past_len), dtype=inputs["input_ids"].dtype
+                )
+
+        if "attention_mask" in self.input_names:
+            inputs["attention_mask"] = attention_mask
+
+        if "token_type_ids" in self.input_names:
+            if token_type_ids is not None:
+                token_type_ids = np.array(token_type_ids)
+            else:
+                token_type_ids = np.zeros(
+                    (input_ids.shape[0], input_ids.shape[1]), dtype=inputs["input_ids"].dtype
+                )
+
+            inputs["token_type_ids"] = token_type_ids
+
+        if hasattr(self, 'next_beam_idx'):
+            inputs['beam_idx'] = self.next_beam_idx
+
+        # Run inference
+        self.request.start_async(inputs, share_inputs=True)
+        self.request.wait()
+        logits = torch.from_numpy(self.request.get_tensor("logits").data).to(self.device)
+
+        if not self.stateful:
+            if self.use_cache:
+                # Tuple of length equal to : number of layer * number of past_key_value per decoder layer (2 corresponds to the self-attention layer)
+                past_key_values = tuple(self.request.get_tensor(key).data for key in self.key_value_output_names)
+                # Tuple of tuple of length `n_layers`, with each tuple of length equal to 2 (k/v of self-attention)
+                past_key_values = tuple(
+                    past_key_values[i : i + self.num_pkv] for i in range(0, len(past_key_values), self.num_pkv)
+                )
+            else:
+                past_key_values = None
+
+        return CausalLMOutputWithPast(logits=logits, past_key_values=past_key_values)
 
 
 model_classes = {
     "mpt": OVMPTModel,
-    "baichuan2": OVBAICHUANModel,
     "chatglm3": OVCHATGLMModel,
-    "gemma": OVModelForCausalLM
+    "gemma": OVModelForCausalLM,
+    "qwen": OVQWENModel,
+    "baichuan2": OVModelForCausalLM,
 }
