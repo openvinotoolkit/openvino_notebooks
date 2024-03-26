b65b685046b912088ffe27b758cbf386b70f2693
yatarkan <yaroslav.tarkan@intel.com>
2024-03-26 19:15:51 +0400

    Move skipped notebooks to separate file, update usages

--- utils/generate_notebooks_index.py
+++ utils/generate_notebooks_index.py
@@ -6,18 +6,20 @@ REPO_ROOT = Path(__file__).resolve().parents[1]
 NOTEBOOKS_DIR = REPO_ROOT / "notebooks"
 INDEX_FILE_NAME = "index.md"
 
-# TODO Move skipped notebooks to separate file (adapt notebook-metadata-handler.js as well)
-SKIPPED_NOTEBOOKS = (
-    "ct-segmentation-quantize/data-preparation-ct-scan.ipynb",
-    "ct-segmentation-quantize/pytorch-monai-training.ipynb",
-)
+
+def get_skipped_notebooks() -> List[str]:
+    skipped_notebooks_file_path = (
+        REPO_ROOT / "selector" / "src" / "notebook-metadata" / "skipped-notebooks.txt"
+    )
+    with open(skipped_notebooks_file_path, "r", encoding="utf-8") as file:
+        return list(map(lambda v: v.strip(), file.readlines()))
 
 
 def get_notebooks_paths() -> List[Path]:
     all_files = NOTEBOOKS_DIR.glob("**/*.ipynb")
     ignored_files = list(NOTEBOOKS_DIR.glob("**/.ipynb_checkpoints/*"))
     ignored_files.extend(NOTEBOOKS_DIR.glob("**/notebook_utils.ipynb"))
-    for skipped_notebook in SKIPPED_NOTEBOOKS:
+    for skipped_notebook in get_skipped_notebooks():
         ignored_files.extend(NOTEBOOKS_DIR.glob(skipped_notebook))
     return sorted(set(all_files) - set(ignored_files))
 
@@ -33,27 +35,35 @@ def write_to_file(file_path: Path, content: str):
         file.write(content)
 
 
-def main():
-    categories_to_notebooks_paths_map: Dict[str, List[Path]] = {}
+def collect_notebooks_category_to_paths_map() -> Dict[str, List[Path]]:
+    result_map = {}
     for notebook_path in get_notebooks_paths():
         categories_tags = get_notebook_tags(notebook_path)["categories"]
         relative_notebook_path = notebook_path.relative_to(NOTEBOOKS_DIR)
         for category in categories_tags:
-            if category in categories_to_notebooks_paths_map:
-                categories_to_notebooks_paths_map[category].append(
-                    relative_notebook_path
-                )
+            if category in result_map:
+                result_map[category].append(relative_notebook_path)
             else:
-                categories_to_notebooks_paths_map[category] = [relative_notebook_path]
+                result_map[category] = [relative_notebook_path]
+    return result_map
 
+
+def format_to_index_markdown(
+    notebooks_category_to_paths_map: Dict[str, List[Path]]
+) -> str:
     md_contents = ["# OpenVINO Notebooks - Categories\n\n"]
-    for category, notebooks_paths in sorted(categories_to_notebooks_paths_map.items()):
+    for category, notebooks_paths in sorted(notebooks_category_to_paths_map.items()):
         md_contents.append(f"## {category}\n\n")
         for notebook_path in notebooks_paths:
             md_contents.append(f"- [{str(notebook_path)}](./{str(notebook_path)})\n")
         md_contents.append("\n")
+    return "".join(md_contents)
 
-    write_to_file(NOTEBOOKS_DIR / INDEX_FILE_NAME, "".join(md_contents))
+
+def main():
+    notebooks_category_to_paths_map = collect_notebooks_category_to_paths_map()
+    index_md_content = format_to_index_markdown(notebooks_category_to_paths_map)
+    write_to_file(NOTEBOOKS_DIR / INDEX_FILE_NAME, index_md_content)
 
 
 if __name__ == "__main__":
