29d64e3167ea6d97a8ebc7656f7f9a36efa68cf8
yatarkan <yaroslav.tarkan@intel.com>
2024-03-28 14:35:54 +0400

    Add new content for testing

--- selector/src/shared/generate-notebooks-index.js
+++ selector/src/shared/generate-notebooks-index.js
@@ -36,6 +36,7 @@ function getCategoryToNotebooksMetadataMap(notebooksMetadataMap) {
 function formatToIndexMarkdown(categoryToNotebooksMetadataMap) {
   const mdContents = [
     '# OpenVINO Notebooks\n\n',
+    '# Testing GitHub Action', // TODO Remove after testing
     '[OpenVINO™ Notebooks at GitHub Pages](https://openvinotoolkit.github.io/openvino_notebooks/)\n\n',
   ];
 
