0a5d5b5d376105df39d372d4f62e66c40ed14e5a
Aleksandr Mokrov <aleksandr.mokrov@intel.com>
2024-04-08 11:18:00 +0200

    TripoSR - image 3D reconstruction (#1852)
    [CVS-135201]
    
    ---------
    
    Co-authored-by: Ekaterina Aidova <ekaterina.aidova@intel.com>

--- selector/src/shared/notebook-tags.js
+++ selector/src/shared/notebook-tags.js
@@ -23,6 +23,7 @@ export const TASKS = /** @type {const} */ ({
     FEATURE_EXTRACTION: 'Feature Extraction',
     TEXT_TO_IMAGE_RETRIEVAL: "Text-to-Image Retrieval",
     IMAGE_TO_TEXT_RETRIEVAL: "Image-to-Text Retrieval",
+    IMAGE_TO_3D: "Image-to-3D",
     IMAGE_TO_VIDEO: "Image-to-Video"
   },
   CV: {
