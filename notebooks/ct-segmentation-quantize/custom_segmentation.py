496550369d19d0e56d07edeb5565056d6c015b19
Aidova <ekaterina.aidova@intel.com>
2024-04-14 18:13:17 +0400

    fix rebasing issue

--- notebooks/ct-segmentation-quantize/custom_segmentation.py
+++ notebooks/ct-segmentation-quantize/custom_segmentation.py
@@ -14,6 +14,7 @@ import requests
 r = requests.get(
     url="https://raw.githubusercontent.com/openvinotoolkit/openvino_notebooks/latest/utils/notebook_utils.py",
 )
+open("notebook_utils.py", "w").write(r.text)
 from notebook_utils import segmentation_map_to_overlay
 
 
