ff33cd5e2003f6d49bd2dd904c71c9e54602184b
yatarkan <yaroslav.tarkan@intel.com>
2024-05-10 11:39:29 +0400

    Switch artifact collection to fork

--- selector/src/shared/fetch-notebooks-status.js
+++ selector/src/shared/fetch-notebooks-status.js
@@ -16,7 +16,8 @@ import { NOTEBOOKS_STATUS_FILE_NAME } from './constants.js';
  */
 function getLatestNotebooksStatusArtifactUrl() {
   const artifactsResponse = execSync(
-    `curl -L "https://api.github.com/repos/openvinotoolkit/openvino_notebooks/actions/artifacts?per_page=1&name=${NOTEBOOKS_STATUS_FILE_NAME}"`
+    // TODO Switch back to main repo
+    `curl -L "https://api.github.com/repos/yatarkan/openvino_notebooks/actions/artifacts?per_page=1&name=${NOTEBOOKS_STATUS_FILE_NAME}"`
   ).toString();
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   const artifactsResponseJson = /** @type {ArtifactsResponse} */ (JSON.parse(artifactsResponse));
