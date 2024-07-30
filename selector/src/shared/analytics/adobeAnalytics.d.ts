a30493d0e79113fecfc279062b55f8da8fa81df3
yatarkan <yaroslav.tarkan@intel.com>
2024-07-30 19:41:37 +0400

    Add optional detail argument to track component function

--- selector/src/shared/analytics/adobeAnalytics.d.ts
+++ selector/src/shared/analytics/adobeAnalytics.d.ts
@@ -2,7 +2,7 @@ declare global {
   interface Window {
     wap_tms?: {
       custom?: {
-        trackComponentClick?: (componentName: string, value: string) => void;
+        trackComponentClick?: (componentName: string, value: string, detail?: string) => void;
       };
     };
     wapLocalCode?: string;
