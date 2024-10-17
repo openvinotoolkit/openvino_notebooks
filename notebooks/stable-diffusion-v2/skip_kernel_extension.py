08b7cc85b8e120e99cf711ebc2ba633914966ebe
anzr299 <aamir.nazir@intel.com>
2024-10-17 12:39:50 +0400

    experiment commit

---
+++ notebooks/stable-diffusion-v2/skip_kernel_extension.py
@@ -0,0 +1,19 @@
+# https://github.com/RobbeSneyders/Jupyter-skip-extension/blob/e77e3708ebb37a3ff0e851fd773adc851d7c1d6a/skip_kernel_extension.py
+
+
+def skip(line, cell=None):
+    """Skips execution of the current line/cell."""
+    if eval(line):
+        return
+
+    get_ipython().ex(cell)
+
+
+def load_ipython_extension(shell):
+    """Registers the skip magic when the extension loads."""
+    shell.register_magic_function(skip, "line_cell")
+
+
+def unload_ipython_extension(shell):
+    """Unregisters the skip magic when the extension unloads."""
+    del shell.magics_manager.magics["cell"]["skip"]
