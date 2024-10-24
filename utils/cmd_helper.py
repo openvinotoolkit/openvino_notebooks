2d7f6ccd56cf95479ec62ec943d75952738813a6
Aleksandr Mokrov <aleksandr.mokrov@intel.com>
2024-10-24 11:04:39 +0200

    Fix cmd helper

--- utils/cmd_helper.py
+++ utils/cmd_helper.py
@@ -4,14 +4,19 @@ import sys
 from pathlib import Path
 
 
-def clone_repo(repo_url: str, revision: str = None, add_to_sys_path: bool = True):
+def clone_repo(repo_url: str, revision: str = None, add_to_sys_path: bool = True) -> Path:
     repo_path = Path(repo_url.split("/")[-1].replace(".git", ""))
 
     if not repo_path.exists():
-        exit_code = os.system(f"git clone {repo_url}")
-        if exit_code != 0:
-            raise Exception("Failed to clone the repository!")
+        try:
+            subprocess.run(["git", "clone", repo_url], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        except Exception as exc:
+            print(f"Failed to clone the repository: {exc.stderr}")
+            raise
+
         if revision:
             subprocess.Popen(["git", "checkout", revision], cwd=str(repo_path))
     if add_to_sys_path:
         sys.path.insert(0, str(repo_path))
+
+    return repo_path
