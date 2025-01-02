1ab43f1141ae442f371e827df8e109a9979c173f
Ethan Yang <ethan.yang@intel.com>
2025-01-02 22:32:57 +0800

    Update glmv_helper.py
    To align with the modelling update:
    
    https://huggingface.co/THUDM/glm-edge-v-5b/commit/595da783cdf468bf0616b9c05757e911676b2f39

--- notebooks/glm-edge-v/glmv_helper.py
+++ notebooks/glm-edge-v/glmv_helper.py
@@ -316,6 +316,7 @@ def convert_glmv_model(model_id, output_dir, quantization_config):
         pkv = model.model(
             input_ids=input_ids,
             attention_mask=torch.ones((2, 2), dtype=torch.int64),
+            images = torch.zeros([1, 3, image_size, image_size])
         )[1]
         model.forward = types.MethodType(_glmv_transformer_forward, model)
 
