6bca4d497fdc3f06b0ab4db7723df1e45a864e9a
eaidova <ekaterina.aidova@intel.com>
2025-01-08 18:25:21 +0400

    fix style

--- notebooks/grammar-correction/utils.py
+++ notebooks/grammar-correction/utils.py
@@ -97,10 +97,7 @@ def get_quantized_pipeline(
     grammar_corrector_model_int8.decoder_with_past.request = None
     grammar_corrector_model_int8.decoder_with_past._compile()
     grammar_corrector_pipe_int8 = pipeline(
-        "text2text-generation",
-        model=grammar_corrector_model_int8,
-        tokenizer=grammar_corrector_tokenizer,
-        device=torch.device("cpu")
+        "text2text-generation", model=grammar_corrector_model_int8, tokenizer=grammar_corrector_tokenizer, device=torch.device("cpu")
     )
 
     return grammar_corrector_pipe_int8
