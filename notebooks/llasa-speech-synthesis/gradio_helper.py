d4748e1fce2c79ccf41a2b858c0978966d7855a4
eaidova <ekaterina.aidova@intel.com>
2025-03-04 09:39:11 +0400

    add discription

--- notebooks/llasa-speech-synthesis/gradio_helper.py
+++ notebooks/llasa-speech-synthesis/gradio_helper.py
@@ -1,18 +1,20 @@
 import torch
 import gradio as gr
 
+
 def ids_to_speech_tokens(speech_ids):
- 
+
     speech_tokens_str = []
     for speech_id in speech_ids:
         speech_tokens_str.append(f"<|s_{speech_id}|>")
     return speech_tokens_str
 
+
 def extract_speech_ids(speech_tokens_str):
- 
+
     speech_ids = []
     for token_str in speech_tokens_str:
-        if token_str.startswith('<|s_') and token_str.endswith('|>'):
+        if token_str.startswith("<|s_") and token_str.endswith("|>"):
             num_str = token_str[4:-2]
 
             num = int(num_str)
@@ -23,36 +25,29 @@ def extract_speech_ids(speech_tokens_str):
 
 
 def make_demo(ov_model, tokenizer, codec_model):
-    speech_end_id = tokenizer.convert_tokens_to_ids('<|SPEECH_GENERATION_END|>')
+    speech_end_id = tokenizer.convert_tokens_to_ids("<|SPEECH_GENERATION_END|>")
+
     def infer(input_text):
         formatted_text = f"<|TEXT_UNDERSTANDING_START|>{input_text}<|TEXT_UNDERSTANDING_END|>"
 
         # Tokenize the text
-        chat = [
-            {"role": "user", "content": "Convert the text to speech:" + formatted_text},
-            {"role": "assistant", "content": "<|SPEECH_GENERATION_START|>"}
-        ]
+        chat = [{"role": "user", "content": "Convert the text to speech:" + formatted_text}, {"role": "assistant", "content": "<|SPEECH_GENERATION_START|>"}]
 
-        input_ids = tokenizer.apply_chat_template(
-            chat, 
-            tokenize=True, 
-            return_tensors='pt', 
-            continue_final_message=True
-        )
+        input_ids = tokenizer.apply_chat_template(chat, tokenize=True, return_tensors="pt", continue_final_message=True)
         outputs = ov_model.generate(
             input_ids,
             max_length=2048,
-            eos_token_id= speech_end_id ,
-            do_sample=True,    
-            top_p=1,           #  Adjusts the diversity of generated content
-            temperature=0.8,   #  Controls randomness in output
+            eos_token_id=speech_end_id,
+            do_sample=True,
+            top_p=1,  #  Adjusts the diversity of generated content
+            temperature=0.8,  #  Controls randomness in output
         )
         # Extract the speech tokens
-        generated_ids = outputs[0][input_ids.shape[1]:-1]
+        generated_ids = outputs[0][input_ids.shape[1] : -1]
 
-        speech_tokens = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)   
+        speech_tokens = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)
 
-        # Convert  token <|s_23456|> to int 23456 
+        # Convert  token <|s_23456|> to int 23456
         speech_tokens = extract_speech_ids(speech_tokens)
 
         speech_tokens = torch.tensor(speech_tokens).unsqueeze(0).unsqueeze(0)
@@ -75,5 +70,5 @@ def make_demo(ov_model, tokenizer, codec_model):
             ],
             outputs=[audio_output],
         )
-    
+
     return demo
