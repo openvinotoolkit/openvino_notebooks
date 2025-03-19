ba862d899cc25782d862ce78dae7fcb82c6f4b85
eaidova <ekaterina.aidova@intel.com>
2025-03-19 14:20:06 +0400

    fix code style

--- notebooks/got-ocr2/gradio_helper.py
+++ notebooks/got-ocr2/gradio_helper.py
@@ -71,15 +71,15 @@ def render_ocr_text(text, result_path, format_text=False):
         tk = verovio.toolkit()
         tk.loadData(text)
         tk.setOptions(
-        {
-        "pageWidth": 2100,
-        "pageHeight": 800,
-        "footer": "none",
-        "barLineWidth": 0.5,
-        "beamMaxSlope": 15,
-        "staffLineWidth": 0.2,
-        "spacingStaff": 6,
-        }
+            {
+                "pageWidth": 2100,
+                "pageHeight": 800,
+                "footer": "none",
+                "barLineWidth": 0.5,
+                "beamMaxSlope": 15,
+                "staffLineWidth": 0.2,
+                "spacingStaff": 6,
+            }
         )
         tk.getPageCount()
         svg = tk.renderToSVG()
@@ -127,10 +127,7 @@ def render_ocr_text(text, result_path, format_text=False):
             gt = ""
             for out in outputs_list:
                 if out:
-                    if (
-                        "\\begin{tikzpicture}" not in out
-                        and "\\end{tikzpicture}" not in out
-                    ):
+                    if "\\begin{tikzpicture}" not in out and "\\end{tikzpicture}" not in out:
                         while out[-1] == " ":
                             out = out[:-1]
                             if out is None:
@@ -152,6 +149,7 @@ def render_ocr_text(text, result_path, format_text=False):
         with open(result_path, "w") as web_f_new:
             web_f_new.write(new_web)
 
+
 def download_examples():
     example_images = {
         "sheet_music.png": "https://huggingface.co/datasets/hf-internal-testing/fixtures_got_ocr/resolve/main/sheet_music.png",
@@ -162,6 +160,7 @@ def download_examples():
     for file_name, url in example_images.items():
         if not Path(file_name).exists():
             Image.open(requests.get(url, stream=True).raw).save(file_name)
+
 
 def download_rendering_tools():
     if not Path("content-mmd-to-html.html").exists():
@@ -174,6 +173,7 @@ def download_rendering_tools():
         with open("tikz.html", "w") as f:
             f.write(r.text)
 
+
 def make_demo(model, processor):
     UPLOAD_FOLDER = "./uploads"
     RESULTS_FOLDER = "./results"
@@ -181,13 +181,14 @@ def make_demo(model, processor):
     for folder in [UPLOAD_FOLDER, RESULTS_FOLDER]:
         if not os.path.exists(folder):
             os.makedirs(folder)
-    
+
     def cleanup_old_files():
         current_time = time.time()
         for folder in [UPLOAD_FOLDER, RESULTS_FOLDER]:
             for file_path in Path(folder).glob("*"):
                 if current_time - file_path.stat().st_mtime > 3600:  # 1 hour
                     file_path.unlink()
+
     cleanup_old_files()
     download_rendering_tools()
     download_examples()
@@ -209,9 +210,7 @@ def make_demo(model, processor):
                     composite_image = img.get("composite")
                     if composite_image is not None:
                         if isinstance(composite_image, np.ndarray):
-                            cv2.imwrite(
-                                image_path, cv2.cvtColor(composite_image, cv2.COLOR_RGB2BGR)
-                            )
+                            cv2.imwrite(image_path, cv2.cvtColor(composite_image, cv2.COLOR_RGB2BGR))
                         elif isinstance(composite_image, Image.Image):
                             composite_image.save(image_path)
                         else:
@@ -311,9 +310,7 @@ def make_demo(model, processor):
                     )
                     ocr_type = "format"
                 elif task == "Multi-page OCR":
-                    inputs = processor(
-                        image, return_tensors="pt", multi_page=True, format=True
-                    )
+                    inputs = processor(image, return_tensors="pt", multi_page=True, format=True)
                     generate_ids = model.generate(
                         **inputs,
                         do_sample=False,
@@ -340,7 +337,6 @@ def make_demo(model, processor):
             if os.path.exists(image_path):
                 os.remove(image_path)
 
-
     def update_image_input(task):
         if task == "Fine-grained OCR (Color)":
             return (
@@ -366,7 +362,6 @@ def make_demo(model, processor):
                 gr.update(visible=False),
                 gr.update(visible=False),
             )
-
 
     def update_inputs(task):
         if task in [
@@ -422,11 +417,8 @@ def make_demo(model, processor):
                 gr.update(visible=True),
             ]
 
-
     def ocr_demo(image, task, ocr_type, ocr_box, ocr_color):
-        res, html_content, unique_id = process_image(
-            image, task, ocr_type, ocr_box, ocr_color
-        )
+        res, html_content, unique_id = process_image(image, task, ocr_type, ocr_box, ocr_color)
 
         if isinstance(res, str) and res.startswith("Error:"):
             return res, None
@@ -443,8 +435,6 @@ def make_demo(model, processor):
             return formatted_res, f"{download_link}<br>{iframe}"
         return formatted_res, None
 
-
-
     with gr.Blocks(theme=gr.themes.Soft()) as demo:
         gr.Markdown(title)
         gr.Markdown(description)
@@ -453,12 +443,8 @@ def make_demo(model, processor):
             with gr.Column(scale=1):
                 with gr.Group():
                     image_input = gr.Image(type="filepath", label="Input Image")
-                    gallery_input = gr.Gallery(
-                        type="filepath", label="Input images", visible=False
-                    )
-                    image_editor = gr.ImageEditor(
-                        label="Image Editor", type="pil", visible=False
-                    )
+                    gallery_input = gr.Gallery(type="filepath", label="Input images", visible=False)
+                    image_editor = gr.ImageEditor(label="Image Editor", type="pil", visible=False)
                     task_dropdown = gr.Dropdown(
                         choices=[
                             "Plain Text OCR",
@@ -471,26 +457,20 @@ def make_demo(model, processor):
                         label="Select Task",
                         value="Plain Text OCR",
                     )
-                    ocr_type_dropdown = gr.Dropdown(
-                        choices=["ocr", "format"], label="OCR Type", visible=False
-                    )
+                    ocr_type_dropdown = gr.Dropdown(choices=["ocr", "format"], label="OCR Type", visible=False)
                     ocr_box_input = gr.Textbox(
                         label="OCR Box (x1,y1,x2,y2)",
                         placeholder="[100,100,200,200]",
                         visible=False,
                     )
-                    ocr_color_dropdown = gr.Dropdown(
-                        choices=["red", "green", "blue"], label="OCR Color", visible=False
-                    )
+                    ocr_color_dropdown = gr.Dropdown(choices=["red", "green", "blue"], label="OCR Color", visible=False)
                     # with gr.Row():
                     # max_new_tokens_slider = gr.Slider(50, 500, step=10, value=150, label="Max New Tokens")
                     # no_repeat_ngram_size_slider = gr.Slider(1, 10, step=1, value=2, label="No Repeat N-gram Size")
 
                     submit_button = gr.Button("Process", variant="primary")
                     editor_submit_button = gr.Button("Process Edited Image", visible=False, variant="primary")
-                    gallery_submit_button = gr.Button(
-                        "Process Multiple Images", visible=False, variant="primary"
-                    )
+                    gallery_submit_button = gr.Button("Process Multiple Images", visible=False, variant="primary")
 
             with gr.Column(scale=1):
                 with gr.Group():
@@ -610,5 +590,5 @@ def make_demo(model, processor):
             outputs=[output_markdown, output_html],
             label="Fine-grained example",
         )
-    
+
     return demo
