47f25f00d366527b4e90b35d73348005a04a94b7
eaidova <ekaterina.aidova@intel.com>
2025-03-24 17:47:27 +0400

    fix int8 pipeline init on arm

--- notebooks/grammar-correction/utils.py
+++ notebooks/grammar-correction/utils.py
@@ -87,6 +87,7 @@ def get_quantized_pipeline(
     quantized_model_path: Path,
     device: str,
     calibration_dataset_size=CALIBRATION_DATASET_SIZE,
+    additional_args=None,
 ):
     # Get quantized OV model
     quantized_model = quantize(grammar_corrector_pipe, core, quantized_model_path, calibration_dataset_size)
@@ -96,7 +97,8 @@ def get_quantized_pipeline(
     grammar_corrector_model_int8.decoder.model = quantized_model
     grammar_corrector_model_int8.decoder.request = None
     grammar_corrector_model_int8.decoder._compile()
-    grammar_corrector_pipe_int8 = pipeline("text2text-generation", model=grammar_corrector_model_int8, tokenizer=grammar_corrector_tokenizer)
+    additional_args = additional_args or {}
+    grammar_corrector_pipe_int8 = pipeline("text2text-generation", model=grammar_corrector_model_int8, tokenizer=grammar_corrector_tokenizer, **additional_args)
 
     return grammar_corrector_pipe_int8
 
