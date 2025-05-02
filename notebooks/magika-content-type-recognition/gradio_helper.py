77197701714e4129cbb1a04c1c9f482336dedd7f
eaidova <ekaterina.aidova@intel.com>
2025-05-02 10:18:28 +0400

    move remaining gradio demos to helpers

---
+++ notebooks/magika-content-type-recognition/gradio_helper.py
@@ -0,0 +1,26 @@
+import gradio as gr
+
+
+def make_demo(ov_magika):
+    def classify(file_path):
+        """Classify file using classes listing.
+        Args:
+            file_path): path to input file
+        Returns:
+            (dict): Mapping between class labels and class probabilities.
+        """
+        results = ov_magika.identify_bytes_topk(file_path)
+
+        return {result.output.ct_label: float(result.score) for result in results}
+
+
+    demo = gr.Interface(
+        fn=classify,
+        inputs=[
+            gr.File(label="Input file", type="binary"),
+        ],
+        outputs=gr.Label(label="Result"),
+        examples=[["./README.md"]],
+        allow_flagging="never",
+    )
+    return demo
---
+++ notebooks/surya-line-level-text-detection/gradio_helper.py
@@ -0,0 +1,25 @@
+import gradio as gr
+from surya.detection import batch_text_detection
+from PIL import ImageDraw
+
+def make_demo(model, processor, test_image):
+    def predict(image):
+        predictions = batch_text_detection([image], model, processor)
+
+        image = image.copy()
+        draw = ImageDraw.Draw(image)
+
+        for polygon_box in predictions[0].bboxes:
+            draw.rectangle(polygon_box.bbox, width=1, outline="red")
+
+        return image
+
+
+    demo = gr.Interface(
+        predict,
+        gr.Image(label="Image", type="pil", format="pil"),
+        gr.Image(label="Result"),
+        examples=[test_image],
+    )
+
+    return demo
