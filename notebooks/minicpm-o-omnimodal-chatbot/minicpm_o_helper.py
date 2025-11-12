0df23e5fdf77988168e7ba85d21b92acc9dcff7f
Aleksandr Mokrov <aleksandr.mokrov@intel.com>
2025-11-13 00:06:16 +0100

    Use model from ckpt dir, not from cache

--- notebooks/minicpm-o-omnimodal-chatbot/minicpm_o_helper.py
+++ notebooks/minicpm-o-omnimodal-chatbot/minicpm_o_helper.py
@@ -651,7 +651,7 @@ def convert_minicpmo26(model_id, remove_checkpoint=False):
         snapshot_download(model_id, local_dir=ckpt, force_download=True)
         patch_model_code(ckpt)
     model = AutoModel.from_pretrained(
-        model_id,
+        ckpt,
         trust_remote_code=True,
         attn_implementation="sdpa",  # sdpa or flash_attention_2
         torch_dtype=torch.float32,
