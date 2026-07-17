fdcca51289ebca1de9a14409c13e95b39ddfb03d
Andrei Kashchikhin <andrey.kashchikhin@intel.com>
2026-07-17 10:26:46 +0200

    review comments, revert OS names

--- .ci/validation_config.py
+++ .ci/validation_config.py
@@ -4,7 +4,7 @@ from typing import Optional, TypedDict
 
 
 class ValidationMatrix:
-    os = ("linux", "windows", "mac")
+    os = ("ubuntu-22.04", "windows-2022", "macos-13")
     python = ("3.10", "3.11", "3.12", "3.13")
     device = ("cpu", "gpu", "npu")
 
